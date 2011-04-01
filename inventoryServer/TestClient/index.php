a56cb03b19630fd1cec7d3f3b91243de30141d5c
msuter <msuter@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-04-01 07:29:00 +0000

    added Testclient for Zend Inventroy Server
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@105 035473d3-0685-4573-a6ed-08f8c3112c85

---
+++ inventoryServer/TestClient/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<script type="text/javascript" src="/swissmon_development_postgres/web_root/public/js/lib/jquery/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="/swissmon_development_postgres/web_root/public/js/lib/jquery/jquery-ui-1.8.6.custom.min.js"></script>
+<script type="text/javascript" src="/swissmon_development_postgres/web_root/public/js/lib/jquery/jquery-texotela-numeric.js"></script>
+<script type="text/javascript" src="/swissmon_development_postgres/web_root/public/js/util.js"></script>
+
+<script type="text/javascript" src="inventory.js" ></script>
+
+</head>
+
+<body>
+<div>
+<p>Swissmon Alpha</p>
+</div>
+
+<div id="inventories"></div>
+
+<script type="text/javascript">
+
+	initInventory();
+
+</script>
+</body>
+</html>
