217ea69ed64a610dad08e56383c66070822eb001
slr <slr@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-04-11 12:03:13 +0000

    integrated area creation
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@171 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/area/area.new.map.tpl.php
+++ application/modules/area/area.new.map.tpl.php
@@ -1,22 +1,18 @@
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?key=ABQIAAAABuqUv_uyCZ4WzUTgK5G-thR8vyPbVAPvWWUSjekUdI5ADbIJJRSNaY0WIlEy744RJmMGHB5KrWGGKw&sensor=false"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/v3_epoly_sphericalArea.js"></script>
 <script type="text/javascript" src="../inventoryServer/web_root/public/css/overlay-style.js"></script>
 <script type="text/javascript" src="../inventoryServer/web_root/public/js/lib/jquery/jquery-ui-1.8.6.custom.min.js"></script>
-<script type="text/javascript" src="../inventoryServer/web_root/public/js/lib/jquery/jquery.dataTables.js"></script>
-<script type="text/javascript" src="../inventoryServer/web_root/public/js/lib/jquery/jquery.dataTables.pluginAPI.js"></script>
 <script type="text/javascript" src="modules/inventory/js/lib/jquery/ui.geo_autocomplete.js"></script>
 <script type="text/javascript" src="modules/inventory/js/util.js"></script>
 <script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/Config.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/FormWindow.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/GeometryOverlay.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/GeometryOverlayControl.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MapAjaxProxy.js"></script>
 <script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MapCache.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MapGeometryOverlays.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/Marker.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MarkerControl.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/Polygon.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MapAjaxProxy.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/GeometryOverlayControl.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/GeometryOverlay.js"></script>
 <script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/PolygonControl.js"></script>
-<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/v3_epoly_sphericalArea.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/Polygon.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/MarkerControl.js"></script>
+<script type="text/javascript" src="modules/area/js/lib/rwo_gmaps/Marker.js"></script>
 <script type="text/javascript" src="modules/area/js/area.js"></script>
 
 <dt id="map_search-label">
@@ -31,16 +27,20 @@
 
 <div id="map">
 	<div id="map_canvas" style="height: 100%"></div>
-	<button id="continue">Weiter...</button>
+	<button id="continue" class="form-submit"><?php echo t('Continue...'); ?></button>
 </div>
 
 <div style="display: none">
-	<form id="Area" enctype="application/x-www-form-urlencoded" method="post" action="<?php drupal_get_path('module', 'area_new_prop')?>">
-		<input type="text" name="locality" id="locality" value="" /> 
-		<input type="text" name="zip" id="zip" value="" />
-		<input type="text" name="township" id="township" value="" />
-		<input type="text" name="canton" id="canton" value="" />
-		<input type="text" name="altitude" id="altitude" value="" />
-		<textarea name="comment" id="comment" cols="60" rows="10"></textarea>
+	<form id="Area" enctype="application/x-www-form-urlencoded" method="post" action="area_new_prop">
+		<input type="hidden" name="locality" id="locality" value="" /> 
+		<input type="hidden" name="zip" id="zip" value="" />
+		<input type="hidden" name="township" id="township" value="" />
+		<input type="hidden" name="canton" id="canton" value="" />
+		<input type="hidden" name="altitude" id="altitude" value="" />
+		<input type="hidden" name="surface_area" id="surface_area" value="" />
+		<input type="hidden" name="latitude" id="latitude" value="" />
+		<input type="hidden" name="longitude" id="longitude" value="" />
+		<input type="hidden" name="area_coords" id="area_coords" value="" />
+		<input type="hidden" name="area_type" id="area_type" value="" />
 	</form>
 </div>
--- application/modules/area/js/area.js
+++ application/modules/area/js/area.js
@@ -26,20 +26,42 @@ function Area() {
 		me.overlayControl.startDigitizing();
 
 		// set continue script
+		var gotAltitude = false;
+		var gotAddress = false;
 		jQuery('#continue').click(function() {
 			if (me.overlayControl.overlay != null) {
 				jQuery('#surface_area').val(me.overlayControl.overlay.getArea());
 				me.overlayControl.overlay.getAltitude(function(altitude) {
 					jQuery('#altitude').val(altitude);
+					gotAltitude = true;
+					if (gotAddress) {
+						jQuery('#Area').submit();
+					}
 				});
 				me.overlayControl.overlay.getAddress(function(address) {
 					jQuery('#canton').val(address.canton);
 					jQuery('#township').val(address.township);
 					jQuery('#locality').val(address.locality);
 					jQuery('#zip').val(address.zip);
+					gotAddress = true;
+					if (gotAltitude) {
+						jQuery('#Area').submit();
+					}
 				});
-				jQuery('#Area').submit();
-				return true;
+				var centGLatLng = me.overlayControl.overlay.getCenter();
+				jQuery('#latitude').val(centGLatLng.lat());
+				jQuery('#longitude').val(centGLatLng.lng());
+				var area_coords = new Array();
+				var markers = me.overlayControl.overlay.markers;
+				for (var i in markers) {
+					area_coords.push(new Array(markers[i].position.lat(), markers[i].position.lng()));
+				}
+				area_coords = JSON.stringify(area_coords);
+				
+				jQuery('#area_coords').val(area_coords);
+				jQuery('#area_type').val(me.overlayControl.overlay.type);
+				
+				return false;
 			} else {
 				alert("Bitte zuerst ein Gebiet eintragen.");
 			}
@@ -47,158 +69,7 @@ function Area() {
 		});
 
 		util.geocode('map_search', 'map_search_button', map);
-
-		// save and go on to create an inventory
-		jQuery('#save_to_inventory').click(function() {
-			var area = me.formToJson();
-			jQuery.post("area-create/save", area, function(response) {
-				// TODO: Change!
-				console.info(response);
-				if (response.id != 'undefined') {
-					window.location.href = 'inventory?area_id=' + response.id;
-				}
-				alert('speichern erfolgreich\n' + response.id);
-			});
-		});
-
-		// save and create new area
-		jQuery('#save_new_area').click(function() {
-			var area = me.formToJson();
-			jQuery.post("area-create/save", area, function(response) {
-				// TODO: Change!
-				if (response.id != 'undefined') {
-					window.location.href = 'area-create';
-				}
-				alert('speichern erfolgreich\n' + response.id);
-			});
-		});
-
-		// Habitat Autocomplete
-		/*
-		 * jQuery("#habitat").autocomplete({ source: "area-create/get-habitats",
-		 * focus: function( event, ui ) { console.info(ui); jQuery( "#habitat"
-		 * ).val( ui.item.label+" "+ui.item.name ); return false; }, select:
-		 * function(event, ui) { jQuery('#habitat').val(ui.item.label+"
-		 * "+ui.item.name);
-		 * jQuery('#habitat_id_'+me.habitatsCnt).val(ui.item.id);
-		 * me.setHabitat(ui.item); return false; }, minLength: 1, change:
-		 * function (event, ui) { console.info('change'); //if the value of the
-		 * textbox does not match a suggestion, clear its value if
-		 * (jQuery(".ui-autocomplete li:textEquals('"+
-		 * jQuery(this).val().replace(/([{}\(\)\^$&.\*\?\/\+\|\[\\\\]|\]|\-)/g,
-		 * '\\$1') + "')").size() == 0) { jQuery(this).val('');
-		 * jQuery('#habitat_id_'+me.habitatsCnt).val(''); } else {
-		 * me.setHabitat(ui.item); } } }).live('keydown', function (e) { var
-		 * keyCode = e.keyCode || e.which; //if TAB or RETURN is pressed and the
-		 * text in the textbox does not match a suggestion, set the value of the
-		 * textbox to the text of the first suggestion if((keyCode == 9 ||
-		 * keyCode == 13)) { if(jQuery(".ui-autocomplete li:textEquals('"+
-		 * jQuery('#habitat').val().replace(/([{}\(\)\^$&.\*\?\/\+\|\[\\\\]|\]|\-)/g,
-		 * '\\$1') + "')").size() == 0) { jQuery(this).val('');
-		 * jQuery('#habitat_id_'+me.habitatsCnt).val('');
-		 *  } else { var item =
-		 * jQuery("#habitat").data('autocomplete').selectedItem;
-		 * me.setHabitat(item); jQuery('#habitat').focus(); } } }).data(
-		 * "autocomplete" )._renderItem = function( ul, item ) { //highlighting
-		 * of matches var re_label = new RegExp("^"+this.term, 'i'); var term =
-		 * this.term.replace(/[aou]/, function (m){ // to highlight with term
-		 * 'wasser' -> 'wasser' and 'gewässer' var hash = { 'a' : '(ä|a)', 'o' :
-		 * '(ö|o)', 'u' : '(ü|u)' }; return hash[m]; }); var re_name = new
-		 * RegExp(term, 'ig'); var label = item.label.replace(re_label ,"<span
-		 * class='ui-term-match'>$&</span>"); var name =
-		 * item.name.replace(re_name,"<span class='ui-term-match'>$&</span>");
-		 * 
-		 * return jQuery( "<li></li>" ) .data( "item.autocomplete", item )
-		 * .append( "<a><div class='ui-menu-item-label'>" + label + "</div><div
-		 * class='ui-menu-item-name'>" + name + "</div></a>" ) .appendTo( ul ); };
-		 */
 	});
-
-};
-
-/**
- * Create Json Array from form values
- * 
- * @return Json
- */
-Area.prototype.formToJson = function() {
-	var me = this;
-	var area_coords;
-	var centroid;
-	if (me.overlayControl.overlay != null) {
-		area_coords = me.overlayControl.overlay.toJson();
-		var centGLatLng = me.overlayControl.overlay.getCenter();
-		centroid = {
-			'lat' : centGLatLng.lat(),
-			'lng' : centGLatLng.lng()
-		};
-	} else {
-		// TODO: Change!
-		alert('Kein Gebiet eingetragen!');
-	}
-
-	var habitat_ids = {};
-	jQuery.each(jQuery('input[name^="habitat_id"]'), function(i, val) {
-		if (val.value !== '') {
-			habitat_ids[i] = val.value;
-		}
-	});
-
-	var area = {
-		'field_name' : jQuery('#field_name').val(),
-		'parcel_nr' : jQuery('#parcel_nr').val(),
-		'altitude' : jQuery('#altitude').val(),
-		'locality' : jQuery('#locality').val(),
-		'zip' : jQuery('#zip').val(),
-		'township' : jQuery('#township').val(),
-		'canton' : jQuery('#canton').val(),
-		'surface_area' : jQuery('#surface_area').val(),
-		'centroid' : centroid,
-		'habitats' : habitat_ids,
-		'comment' : jQuery('#comment').val(),
-		'area' : area_coords
-	};
-	console.info(area);
-	return area;
-};
-
-/**
- * Sets a habitat for area
- */
-Area.prototype.setHabitat = function(item) {
-	var me = this;
-	var habitat = jQuery('#habitat');
-	var habitat_table = me.getHabitatTable(habitat);
-
-	habitat_table.append("<tr id='habitat-tr_" + this.habitatsCnt + "'><td>" + item.label + "</td><td>" + item.name
-			+ "</td><td><img id='habitat-del_" + this.habitatsCnt + "' src='images/can_delete.png' /></td></tr>");
-
-	jQuery('#habitat-del_' + this.habitatsCnt).bind('click', {
-		'entry' : this.habitatsCnt
-	}, (function(e) {
-		jQuery('#habitat-tr_' + e.data.entry).remove();
-		jQuery('#habitat_id_' + e.data.entry).remove();
-	}));
-
-	habitat.val('');
-	var habitat_id = jQuery('#habitat_id_' + me.habitatsCnt);
-	me.habitatsCnt++;
-	var new_habitat_id = habitat_id.clone();
-	new_habitat_id.val('');
-	new_habitat_id.attr('id', "habitat_id_" + me.habitatsCnt);
-	habitat_id.after(new_habitat_id);
-	me.curHabitatId = new_habitat_id;
-
-};
-
-/**
- * @return html table of habitats for the area
- */
-Area.prototype.getHabitatTable = function(before) {
-	if (jQuery('#habitat_table > tbody:last').length == 0) {
-		before.before("<table id='habitat_table'><tbody></tbody></table>");
-	}
-	return jQuery('#habitat_table > tbody:last');
 };
 
 new Area();
