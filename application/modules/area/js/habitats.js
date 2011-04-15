4860e6db0de1a0de7f3c38029a8f88f09c0a9c79
slr <slr@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-04-15 08:54:01 +0000

    avoid dupplicate habitat entries
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@213 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/area/js/habitats.js
+++ application/modules/area/js/habitats.js
@@ -5,20 +5,26 @@ jQuery(document).ready(function() {
 	jQuery("input#edit-habitattext").bind("autocomplete_select", habitatAutocompleteSelect);
 });
 
+var habitantIds = new Array();
+
 // This will be called if user selects an autocompletion value
 function habitatAutocompleteSelect(event) {
 	// get the new selected value
 	var newHabitat = jQuery(this).val();
-
-	// append it to the table
 	var newHabitatArray = newHabitat.split("|");
-	var id = "<input type='hidden' name='habitat[" + newHabitatArray[0] + "]' value='" + newHabitatArray[0] + "'/>";
-	var label = "<td>" + newHabitatArray[1] + "</td>";
-	var name = "<td>" + newHabitatArray[2] + "</td>";
-	var removeImg = Drupal.settings.basePath + "/modules/inventory/images/can_delete.png";
-	var remove = "<td><img src='" + removeImg + "' onClick='removeHabitat(this);' style='cursor:pointer'/>";
-	jQuery("#habitatSelected table").append("<tr>" + id + label + name + remove + "</tr>");
-	
+
+	// avoid duplicates
+	if (jQuery.inArray(newHabitatArray[0], habitantIds) < 0) {
+		habitantIds.push(newHabitatArray[0]);
+		// append it to the table
+		var id = "<input type='hidden' name='habitat[" + newHabitatArray[0] + "]' value='" + newHabitatArray[0] + "'/>";
+		var label = "<td>" + newHabitatArray[1] + "</td>";
+		var name = "<td>" + newHabitatArray[2] + "</td>";
+		var removeImg = Drupal.settings.basePath + "/modules/inventory/images/can_delete.png";
+		var remove = "<td><img src='" + removeImg + "' onClick='removeHabitat(this);' style='cursor:pointer'/>";
+		jQuery("#habitatSelected table").append("<tr>" + id + label + name + remove + "</tr>");
+	}
+
 	// reset the autocompletion textfield
 	jQuery(this).val("");
 }
@@ -26,5 +32,8 @@ function habitatAutocompleteSelect(event) {
 // This will be called if a user clicks on the remove icon
 function removeHabitat(image) {
 	var row = jQuery(image.parentElement.parentElement);
+	var id = row.find('input').attr('value');
+	var pos = jQuery.inArray(id, habitantIds);
+	habitantIds.splice(pos, 1);
 	row.remove();
 }
