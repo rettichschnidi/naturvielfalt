695ef3e8a1f77f0331bffa7699e1ceb0000a2b79
slr <slr@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-04-29 06:22:22 +0000

    slight restructuring
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@258 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/area/js/area-select.js
+++ application/modules/area/js/area-select.js
@@ -7,169 +7,215 @@ var INVENTORY_SERVER_PATH = '../inventoryServer/web_root/public/';
 
 
 function AreaSelect() {
-    var me = this;
+	
+	// Member variable declarations
+	var me = this; // references to the instance of AreaSelect
+	var bruggCenter = new google.maps.LatLng(47.487084, 8.207273);
+	var selected_area = null; // index of currently selected area
+	var areaInfo = null; // popup bubble shown if user clicks on an area
+	var map = null; // google maps object
+	var mapOverlays = null; // Overlays representing areas
+	
+	/**
+     * This function will be called if the user clicks on the plus or minus symbol at the beginning
+     * of a row.
+     */
+    AreaSelect.prototype.onTableExpanderClicked = function () {
+        var nTr = this.parentNode.parentNode;
+        if ( this.src.match('details_close') ) {
+            /* This row is already open - close it */
+            this.src = "modules/area/images/details_open.png";
+            me.area_table.fnClose( nTr );
+        } else {
+            /* Open this row */
+            this.src = "modules/area/images/details_close.png";
+            me.area_table.fnOpen( nTr, me.formatDetails(nTr), 'details' );
+        }
+    }
+    
+    /**
+     * This function is called when the user clicks on a row
+     */
+    AreaSelect.prototype.onTableRowClicked = function(event) {
+    	jQuery(areaselect.area_table.fnSettings().aoData).each(function() {
+    		jQuery(this.nTr).removeClass('row_selected');
+    		if (this.nTr.getAttribute('overlay_id')) {
+    			areaselect.mapOverlays.overlays[this.nTr.getAttribute('overlay_id')].setStyle('selected-disable');
+    		}
+    	});
+    	jQuery(event.target.parentNode).addClass('row_selected');
+    	var overlayId = event.target.parentNode.getAttribute('overlay_id');
+    	areaselect.mapOverlays.overlays[overlayId].setStyle('selected');
+    	areaselect.selected_area = overlayId;
+    	areaselect.map.panTo(areaselect.mapOverlays.overlays[overlayId].getCenter());
+    }
+    
+    /**
+     * This is called if the user hovers over a table row
+     */
+    AreaSelect.prototype.onTableRowHover = function(event) {
+    	var parent = event.target.parentNode;
+        if(event.type == 'mouseover') {
+            jQuery(parent).addClass( 'row_highlighted' );
+            me.mapOverlays.overlays[parent.getAttribute('overlay_id')].setStyle('highlighted');
+        } else {
+            jQuery(parent).removeClass('row_highlighted');
+            me.mapOverlays.overlays[parent.getAttribute('overlay_id')].setStyle('highlighted-disable');
+        }
+    }
+    
+    /**
+     * Creates the google maps object and attaches it to the element with the id 'map_canvas'.
+     * Returns a google.maps.Map
+     */
+    AreaSelect.prototype.createGoogleMaps = function() {
+    	var mapsOptions = { // Google maps options
+    			zoom : 15,
+    			center : bruggCenter,
+    			mapTypeId : google.maps.MapTypeId.ROADMAP
+    		};
+    	return new google.maps.Map(document.getElementById('map_canvas'), mapsOptions);
+    }
+    
+    /**
+     * Creates the bubble which shows information on an area when the user clicks on a area.
+     * Returns an InfoBubble
+     */
+    AreaSelect.prototype.createAreaInfo = function() {
+    	areaInfo = new InfoBubble({
+    		map : me.map,
+    		shadowStyle : 1,
+    		padding : 8,
+    		borderRadius : 8,
+    		arrowSize : 40,
+    		borderWidth : 1,
+    		borderColor : '#2c2c2c',
+    		disableAutoPan : true,
+    		arrowPosition : 20,
+    		arrowStyle : 2
+    	});
+    	areaInfo.addTab('Details', '');
+    	areaInfo.addTab('Inventories', '');
+    	areaInfo.addTab('Habitats', '');
+    	return areaInfo;
+    }
+    
+    /**
+     * Creates the overlay representing areas
+     * Returns MapGeometryOverlays
+     */
+    AreaSelect.prototype.createOverlays = function() {
+    	var overlays = new MapGeometryOverlays(me.map);
+    	return overlays;
+    }
+    
+    this.map = this.createGoogleMaps();
+	this.areaInfo = this.createAreaInfo();
+	// all overlays shown in the map
+	this.mapOverlays = this.createOverlays();
 
-    google.maps.event.addDomListener(window,"load", function() {
-        //create map
-        var bruggCenter = new google.maps.LatLng(47.487084, 8.207273);
-        var myOptions = {
-            zoom: 15,
-            center: bruggCenter,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        me.selected_area = null;
-        me.map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
-        // all overlays shown in the map
-        me.mapOverlays = new MapGeometryOverlays(me.map);
+	util.geocode('map_search', 'map_search_button', me.map);
 
-        util.geocode('map_search', 'map_search_button', me.map);
+	jQuery('#create-inventory').click(onCreateInventoryClicked);
+
+
+	// select row in table
+	jQuery("#area_table tbody").live('click', this.onTableRowClicked);
+	// hover row in table
+	jQuery('tbody > tr').live('mouseover mouseout', this.onTableRowHover);
         
-        jQuery('#create-inventory').click(function(){
-            if(me.selected_area != null){
-                window.location.href = 'inventory/new/'+me.selected_area;
-             } else {
-                 alert('Bitte wählen Sie ein Gebiet aus.');
-             }
-         });
-        
-        // create info window (bubble) for area information
-        me.areaInfo = new InfoBubble({
-            map: me.map,
-            shadowStyle: 1,
-            padding: 8,
-            borderRadius: 8,
-            arrowSize: 40,
-            borderWidth: 1,
-            borderColor: '#2c2c2c',
-            disableAutoPan: true,
-            arrowPosition: 20,
-            arrowStyle: 2
-          });
-        me.areaInfo.addTab('Details', '');
-        me.areaInfo.addTab('Inventories', '');
-        me.areaInfo.addTab('Habitats', '');
-
-        // select row in table
-        jQuery("#area_table tbody").live('click', function(event) {
-            jQuery(me.area_table.fnSettings().aoData).each( function () {
-                jQuery(this.nTr).removeClass('row_selected');
-                if(this.nTr.getAttribute('overlay_id')) {
-                    me.mapOverlays.overlays[this.nTr.getAttribute('overlay_id')].setStyle('selected-disable');
-                }
-            });
-            jQuery(event.target.parentNode).addClass('row_selected');
-            var overlayId = event.target.parentNode.getAttribute('overlay_id');
-            me.mapOverlays.overlays[overlayId].setStyle('selected');
-            me.selected_area = overlayId;
-            me.map.panTo(me.mapOverlays.overlays[overlayId].getCenter());
-        });
-        // hover row in table
-        jQuery('tbody > tr').live('mouseover mouseout', function(event) {
-        	var parent = event.target.parentNode;
-            if(event.type == 'mouseover') {
-                jQuery(parent).addClass( 'row_highlighted' );
-                me.mapOverlays.overlays[parent.getAttribute('overlay_id')].setStyle('highlighted');
-            } else {
-                jQuery(parent).removeClass('row_highlighted');
-                me.mapOverlays.overlays[parent.getAttribute('overlay_id')].setStyle('highlighted-disable');
-            }
-
-        });
-        
-        // create table
-        me.area_table = jQuery('#area_table').dataTable( {
-            "oLanguage" : {
-                "sUrl" : "modules/area/languages/de_DE/DataTables.txt"
-            },
-            "bScrollInfinite": true,
-            "bScrollCollapse": true,
-            "sScrollY": "400px",
-            "bProcessing": true,
-            "bServerSide": true,
-            "sAjaxSource": INVENTORY_SERVER_PATH + "area-select/get-area",
-            "fnServerData": function ( sSource, aoData, fnCallback ) {
-                /* Add some data to send to the source, and send as 'POST' */
-                //aoData.push( { "name": "my_field", "value": "my_value" } );
-                jQuery.ajax( {
-                    "dataType": 'json',
-                    "type": "POST",
-                    "url": sSource,
-                    "data": aoData,
-                    "success": function (json) {
-                        var aaData = [];
-                        var areas = json.areas;
-                        me.mapOverlays.clear();
-                        
-                        for(var i in areas) {
-                            aaData.push([
-                              '<img src="modules/area/images/details_open.png"></img>',
-                              areas[i]['field_name'], 
-                              areas[i]['locality'],
-                              areas[i]['township'],
-                              areas[i]['canton'],
-                              areas[i]['surface_area'],    
-                              areas[i]['altitude'], 
-                              areas[i]['type'],
-                              areas[i]['parcel_nr'],
-                              me.formatHabitats(areas[i]['habitats']),   
-                              areas[i]['comment'], 
-                              areas[i]['id']]);
-                        }
-                        var newJson = {
-                            'sEcho' : json.sEcho,
-                            'iTotalRecords' : json.iTotalRecords,
-                            'iTotalDisplayRecords': json.iTotalDisplayRecords,
-                            'aaData': aaData
-                        }
-                        me.mapOverlays.addOverlaysJson(json.areas);
-
-                        for(var i in me.mapOverlays.overlays) {
-                            var overlay = me.mapOverlays.overlays[i];
-                            me.addOverlayListener(overlay);
-                        }
-                        fnCallback(newJson);
-                        jQuery(me.area_table.fnSettings().aoData).each( function () {
-                            this.nTr.setAttribute('overlay_id', this._aData[11]);
-                        });
+    // create table
+    me.area_table = jQuery('#area_table').dataTable( {
+        "oLanguage" : {
+            "sUrl" : "modules/area/languages/de_DE/DataTables.txt"
+        },
+        "bScrollInfinite": true,
+        "bScrollCollapse": true,
+        "sScrollY": "400px",
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": INVENTORY_SERVER_PATH + "area-select/get-area",
+        "fnServerData": function ( sSource, aoData, fnCallback ) {
+            /* Add some data to send to the source, and send as 'POST' */
+            //aoData.push( { "name": "my_field", "value": "my_value" } );
+            jQuery.ajax( {
+                "dataType": 'json',
+                "type": "POST",
+                "url": sSource,
+                "data": aoData,
+                "success": function (json) {
+                    var aaData = [];
+                    var areas = json.areas;
+                    me.mapOverlays.clear();
+                    
+                    for(var i in areas) {
+                        aaData.push([
+                          '<img src="modules/area/images/details_open.png"></img>',
+                          areas[i]['field_name'], 
+                          areas[i]['locality'],
+                          areas[i]['township'],
+                          areas[i]['canton'],
+                          areas[i]['surface_area'],    
+                          areas[i]['altitude'], 
+                          areas[i]['type'],
+                          areas[i]['parcel_nr'],
+                          me.formatHabitats(areas[i]['habitats']),   
+                          areas[i]['comment'], 
+                          areas[i]['id']]);
                     }
-                } );
-            },
-            "aoColumns": [
-            {"sClass": "center", "bSortable": false,  "bSearchable" : false},
-            {"sTitle": "Flurname"},
-            {"sTitle": "Ortschaft"},
-            {"sTitle": "Gemeinde"},
-            {"sTitle": "Kanton"},
-            {"sTitle": "Fläche (m&#178;)"},
-            {"sTitle": "M.ü.M"},
-            {"sTitle": "Typ", "bSearchable" : false, "bSortable": false},
-            {"sTitle": "ParzellenNr.", "bSearchable" : false, "bVisible": false},
-            {"sTitle": "Lebensräume", "bSearchable" : false, "bVisible": false},
-            {"sTitle": "Beschreibung", "bSearchable" : false, "bVisible": false},
-            {"sTitle": "area_id", "bSearchable" : false, "bVisible": false}
-            ],
-            "aaSorting": [[1, 'asc']],
-            'fnInitComplete': function (){
-              this.fnSetFilteringDelay(250);
-            }
-        } );
+                    var newJson = {
+                        'sEcho' : json.sEcho,
+                        'iTotalRecords' : json.iTotalRecords,
+                        'iTotalDisplayRecords': json.iTotalDisplayRecords,
+                        'aaData': aaData
+                    }
+                    me.mapOverlays.addOverlaysJson(json.areas);
+
+                    for(var i in me.mapOverlays.overlays) {
+                        var overlay = me.mapOverlays.overlays[i];
+                        me.addOverlayListener(overlay);
+                    }
+                    fnCallback(newJson);
+                    jQuery(me.area_table.fnSettings().aoData).each( function () {
+                        this.nTr.setAttribute('overlay_id', this._aData[11]);
+                    });
+                }
+            } );
+        },
+        "aoColumns": [
+        {"sClass": "center", "bSortable": false,  "bSearchable" : false},
+        {"sTitle": "Flurname"},
+        {"sTitle": "Ortschaft"},
+        {"sTitle": "Gemeinde"},
+        {"sTitle": "Kanton"},
+        {"sTitle": "Fläche (m&#178;)"},
+        {"sTitle": "M.ü.M"},
+        {"sTitle": "Typ", "bSearchable" : false, "bSortable": false},
+        {"sTitle": "ParzellenNr.", "bSearchable" : false, "bVisible": false},
+        {"sTitle": "Lebensräume", "bSearchable" : false, "bVisible": false},
+        {"sTitle": "Beschreibung", "bSearchable" : false, "bVisible": false},
+        {"sTitle": "area_id", "bSearchable" : false, "bVisible": false}
+        ],
+        "aaSorting": [[1, 'asc']],
+        'fnInitComplete': function (){
+          this.fnSetFilteringDelay(250);
+        }
+    } );
         
-        // detail view of table rows
-        jQuery('#area_table tbody td img').live( 'click', function () {
-            var nTr = this.parentNode.parentNode;
-            if ( this.src.match('details_close') ) {
-                /* This row is already open - close it */
-                this.src = "modules/area/images/details_open.png";
-                me.area_table.fnClose( nTr );
-            } else {
-                /* Open this row */
-                this.src = "modules/area/images/details_close.png";
-                me.area_table.fnOpen( nTr, me.formatDetails(nTr), 'details' );
-            }
-        } );
-        
-    });
+    // detail view of table rows
+    jQuery('#area_table tbody td img').live( 'click', this.onTableExpanderClicked );
 };
+
+/*
+ * This is called if the user clicks on the button "create inventory".
+ */
+function onCreateInventoryClicked() {
+	if (areaselect.selected_area != null) {
+		window.location.href = 'inventory/new/' + areaselect.selected_area;
+	} else {
+		alert('Bitte wählen Sie ein Gebiet aus.');
+	}
+}
 
 /**
  * adds a listener for selecting and hover to geometry overlays
@@ -244,6 +290,11 @@ AreaSelect.prototype.addOverlayListener = function(overlay) {
 		});
 		me.areaInfo.close();
 		me.areaInfo.setPosition(overlays[info.id].getCenter());
+		
+        jQuery('tr[overlay_id="'+info.id+'"]').addClass('row_selected');
+        overlays[info.id].setStyle('selected');
+        me.selected_area = info.id;
+        me.map.panTo(overlays[info.id].getCenter());
     });
 };
 
@@ -269,4 +320,7 @@ AreaSelect.prototype.formatHabitats = function(habitats){
   return sHabitats;
 };
 
-new AreaSelect();
+var areaselect = null;
+jQuery(document).ready(function() {
+	areaselect = new AreaSelect();
+});
