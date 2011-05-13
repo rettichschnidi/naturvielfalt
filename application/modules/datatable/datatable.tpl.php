46d6634dbe3d354a1949bea09a42eb870826448e
sma <sma@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-05-13 14:51:42 +0000

    modification of datatable to do server side processing => not yet completed
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@327 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/datatable/datatable.tpl.php
+++ application/modules/datatable/datatable.tpl.php
@@ -19,6 +19,26 @@ $table[$id_table] = array(
 if($len == NULL) $len = 300;
 
 print drupal_render($table);
+
+/*
+ *
+ * Use ajax to fetch entries:
+			"bServerSide": true,
+			"sAjaxSource": "<?php echo base_path() . 'area/datatable'; ?>",  // module dependant path
+			"fnServerData": function ( sSource, aoData, fnCallback ) {
+				jQuery.ajax( {
+					"dataType": 'json',
+					"type": "POST",
+					"url": sSource,
+					"data": aoData,
+					"success": fnCallback
+				} );
+			},
+ * 
+ */
+
+
+
 ?>
 <script type="text/javascript" charset="utf-8">
 	jQuery(document).ready(function() {
@@ -30,7 +50,7 @@ print drupal_render($table);
 			"bScrollCollapse": true,
 			"sScrollY": "<?php echo $len."px"; ?>",
 			"sDom": '<"ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper-clearfix"if>\
-					t\
+					rt\
 					<"ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix">',
 		});
 	});
