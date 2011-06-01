cf472dccf5ecb84969f49040a604b11875850a20
damian3 <damian3@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-06-01 13:56:47 +0000

    added linked inventories to areas and added collabsable tables added permission check for inventory view in inventory module
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@400 035473d3-0685-4573-a6ed-08f8c3112c85

---
+++ application/modules/area/js/area.datatable.select.js
@@ -0,0 +1,23 @@
+
+// @author Damian Conrad
+
+function inventoryRowSelect() {
+	/**
+	 * This function is called when the user clicks on a row
+	 */
+	
+	inventoryRowSelect.prototype.onTableRowClicked = function(e) {
+		if (!e) var e = window.event;
+		if (e.target) targ = e.target;
+		else if (e.srcElement) targ = e.srcElement;
+		if (targ.nodeType == 3) // defeat Safari bug
+			targ = targ.parentNode;
+		var idType = targ.parentNode.id.split('_');
+		if (idType[0] == 'inventory')
+			window.location.href = '../inventory/'+idType[1];
+	}	
+	// register events
+	jQuery("#inventories").live('click', this.onTableRowClicked);	
+}
+
+new inventoryRowSelect();
