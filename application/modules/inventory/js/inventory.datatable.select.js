6128151f4d2a56af66cad66aac42d9b55d2dbde1
marco6 <marco6@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-06-01 12:43:41 +0000

    display static image overlay in inventories while hovering over the globe symbol
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@399 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/inventory/js/inventory.datatable.select.js
+++ application/modules/inventory/js/inventory.datatable.select.js
@@ -17,13 +17,36 @@ function inventoryRowSelect() {
 			window.location.href = '../organism/'+idType[1];
 		if (idType[0] == 'inventory')
 			window.location.href = 'inventory/'+idType[1];
-	}	
+	}
+	
+	var lastHoverId = 0;
+	inventoryRowSelect.prototype.showStaticImage = function(event) {
+		var posleft = jQuery(event.target.parentNode).position().left+40;
+		var hoverId = event.target.parentNode.id.split('_')[1];
+		var postop = jQuery(event.target.parentNode.parentNode).offset().top - jQuery('#header').height() + jQuery('tbody > tr').height()+5;
+
+		if(event.type == 'mouseover'){
+			jQuery('#static_image').css('display','block');
+			jQuery('#static_image').css('left', posleft);
+			jQuery('#static_image').css('top', postop);
+			
+			if(event.currentTarget.parentNode.id!=lastHoverId){
+				jQuery('#static_image').find('img').attr('src', 'modules/area/images/ajax-loader.gif');
+				jQuery('#static_image').find('img').attr('src', 'area/gmap_image_redirect/'+hoverId);
+			}
+			lastHoverId = hoverId;
+		} else if (event.type == 'mouseout'){
+			jQuery('#static_image').css('display', 'none');
+		}
+	}
+	
 	// register events
 	jQuery("#flora").live('click', this.onTableRowClicked);
 	jQuery("#fauna").live('click', this.onTableRowClicked);
 	jQuery("#inventories_share").live('click', this.onTableRowClicked);
 	jQuery("#inventories_group").live('click', this.onTableRowClicked);
-	jQuery("#inventories_own").live('click', this.onTableRowClicked);	
+	jQuery("#inventories_own").live('click', this.onTableRowClicked);
+	jQuery('.show_static_image').live('mouseover mouseout', this.showStaticImage);
 }
 
 new inventoryRowSelect();
