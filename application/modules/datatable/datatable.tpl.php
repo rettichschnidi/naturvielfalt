f1160d99564cafc81442cce37615f6f08a8a8303
skaeser <skaeser@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-07-13 15:01:39 +0000

    added some more padding to the table rows and implemented possibility to override the options of the jquery datatable plugin over the theme
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@468 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/datatable/datatable.tpl.php
+++ application/modules/datatable/datatable.tpl.php
@@ -4,54 +4,46 @@
  * Using jQuery plugin DataTables
  */
 // add css-file
+drupal_add_library('system', 'ui.datepicker');
+
 drupal_add_css(drupal_get_path('module', 'datatable') . '/css/datatable.css', array('group' => CSS_DEFAULT, 'every_page' => TRUE));
-drupal_add_css(drupal_get_path('module', 'datatable') . '/css/jquery-ui-1.8.4.custom.css', array('group' => CSS_DEFAULT, 'every_page' => TRUE));
 drupal_add_js(drupal_get_path('module', 'datatable') . '/js/lib/jquery.dataTables.js');
 drupal_add_js(drupal_get_path('module', 'datatable') . '/js/lib/jquery.scrollTo-min.js');
 
 $table[$id_table] = array(
-    '#theme' => 'table', 
-	'#header' => $header,
-    '#rows' => $rows,
-	'#attributes' => array('id' => $id_table),
+  '#theme' => 'table', 
+  '#header' => $header,
+  '#rows' => $rows,
+  '#sticky' => false,
+  '#attributes' => array('id' => $id_table),
 );
 
 if($len == NULL) $len = 300;
 
 print drupal_render($table);
 
-/*
- *
- * Use ajax to fetch entries:
-			"bServerSide": true,
-			"sAjaxSource": "<?php echo base_path() . 'area/datatable'; ?>",  // module dependant path
-			"fnServerData": function ( sSource, aoData, fnCallback ) {
-				jQuery.ajax( {
-					"dataType": 'json',
-					"type": "POST",
-					"url": sSource,
-					"data": aoData,
-					"success": fnCallback
-				} );
-			},
- * 
- */
-
-
-
+$additional_options = '';
+if(is_array($options)) {
+  foreach($options as $name => $value) {
+    $additional_options .= ', "'.$name.'": '.$value;
+  }
+}
 ?>
 <script type="text/javascript" charset="utf-8">
-	jQuery(document).ready(function() {
-		oTable = jQuery(<?php echo "'#".$id_table."'"; ?>).dataTable({
-			"bJQueryUI": true,
-			"bPaginate": false,
-			"bAutoWidth": false, 
-			"bScrollInfinite": true,
-			"bScrollCollapse": true,
-			"sScrollY": "<?php echo $len."px"; ?>",
-			"sDom": '<"ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper-clearfix"if>\
-					rt\
-					<"ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix">',
-		});
-	});
+<!--
+  jQuery(document).ready(function() {
+    jQuery(<?php echo "'#".$id_table."'"; ?>).dataTable({
+      "bJQueryUI": true,
+      "bPaginate": false,
+      "bAutoWidth": false, 
+      "bScrollInfinite": true,
+      "bScrollCollapse": true,
+      "sScrollY": "<?php echo $len."px"; ?>",
+      "sDom": '<"ui-toolbar ui-widget-header ui-corner-tl ui-corner-tr ui-helper-clearfix"if>\
+          rt\
+          <"ui-toolbar ui-widget-header ui-corner-bl ui-corner-br ui-helper-clearfix">'
+      <?php echo $additional_options ?>
+    });
+  });
+-->
 </script>
