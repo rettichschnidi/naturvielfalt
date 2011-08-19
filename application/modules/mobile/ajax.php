ad370ac1a30fc08df75e5064f873d79b5557d7dc
fabianvogler <fabianvogler@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-08-19 09:22:31 +0000

    Login
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@632 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/mobile/ajax.php
+++ application/modules/mobile/ajax.php
@@ -1,11 +1,21 @@
 <?php
 
+define('DRUPAL_ROOT', dirname(__FILE__) . '/../..');
+
+require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
 if (!isset($_SERVER['PHP_AUTH_USER'])) {
+
     header('WWW-Authenticate: Basic realm="Naturvielfalt"');
     header('HTTP/1.0 401 Unauthorized');
+
     echo 'Text to send if user hits Cancel button';
     exit;
+
 } else {
-    echo "<p>Hello {$_SERVER['PHP_AUTH_USER']}.</p>";
-    echo "<p>You entered {$_SERVER['PHP_AUTH_PW']} as your password.</p>";
+
+    $uid = user_authenticate($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_USER']);
+
+    var_dump($uid);
 }
