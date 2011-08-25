a8cd1d9569d77574ad7ffedb190476592ca5078a
hre_ethz_svn <hre_ethz_svn@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-08-25 14:04:52 +0000

    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@673 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/open_identification/open_identification_list.tpl.php
+++ application/modules/open_identification/open_identification_list.tpl.php
@@ -1,4 +1,62 @@
+<div class="open_identifications">
+<?php 
 
-<div class="open_identification">
-test
+$string = "";
+foreach($list as $record){
+
+	$medias = $record['medias'];
+	$media  = $medias[0];
+	
+	if($media['filemime'] == 'audio/mpeg')
+		$url = '';
+	elseif($media['filemime'] == 'video/mp4')
+		$url = '';
+	else
+		$url = url('gallery/open_identification/'.$image['item_id'].'/thumb/'.$image['id'].'/gallery_mini');
+		
+	$link   = 'open_identification/'.key($list).'/edit';
+	$string .= '<a href="'.$link.'">';
+	$string .= 	'<div class="open_identification" style=" padding-bottom: 20px;">';
+	$string .= 		'<div class="open_identification_image">';
+	$string .= 			'<img src="'.$url.'">';
+	$string .= 		'</div>';
+	$string .= 		'<div class="open_identification_info" >';
+	$string .= 			'<div class="info" style="float:left; padding-right:40px;">';	
+	$string .= 				'<div class="title">UserId</div>';	
+	$string .= 				'<div class="value">';					
+	$string .= 					$record['info']['user_id'];
+	$string .= 				'</div>';
+	$string .= 			'</div>';
+	$string .= 			'<div class="info" style="float:left; padding-right:40px;">';
+	$string .= 				'<div class="title">create date</div>';	
+	$string .= 				'<div class="value">';					
+	$string .= 					date("d.m.y H:i:s",strtotime($record['info']['create_date']));
+	$string .= 				'</div>';	
+	$string .= 			'</div>';
+	$string .= 			'<div class="info" style="float:left; padding-right:40px;">';
+	$string .= 				'<div class="title">Typ</div>';	
+	$string .= 				'<div class="value">';					
+	$string .= 					t($record['info']['type']);
+	$string .= 				'</div>';	
+	$string .= 			'</div>';
+
+	if(isset($record['info']['organismgroup'])){
+	
+		$string .= 			'<div class="info">';
+		$string .= 				'<div class="title">Organismengruppe</div>';	
+		$string .= 				'<div class="value">';					
+		$string .= 					t($record['info']['organismgroup']);
+		$string .= 				'</div>';	
+		$string .= 			'</div>';
+	}
+
+	$test = "";
+	$string .= 		'</div>';
+	$string .= 	'</div>';
+	$string .= '</a>';
+}
+
+?>
+
+<? echo $string; ?>
 </div>
