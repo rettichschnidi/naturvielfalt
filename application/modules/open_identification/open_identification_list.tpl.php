854ec9cd4a84615681c55cba1bd6c4a3da036ff7
hre_ethz_svn <hre_ethz_svn@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-08-30 11:06:02 +0000

    show open identifications list with type and group titles
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@716 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/open_identification/open_identification_list.tpl.php
+++ application/modules/open_identification/open_identification_list.tpl.php
@@ -1,73 +1,85 @@
-<div class="sort_criteria" ></div>
+<div class="sort_criteria" ><input type="button" onclick="show_prompt()" value="sortieren/filtern" /></div>
 <div class="open_identifications">
 <?php 
 
 $string = "";
 if($list){
-
-		foreach($list as $record){
-		
-			$medias = $record['medias'];
-			$media  = $medias[0];
-			$url="";
-			if($media['filemime'] == 'audio/mpeg'){
-				$url = url(drupal_get_path('module', 'gallery').'/images/music_icon.png');
-			}
-			elseif($media['filemime'] == 'video/mp4'){
-				$url = url(drupal_get_path('module', 'gallery').'/images/video_icon.png');
-			}
-			else{
-				$url_href  = file_uri_target($media['url']);
-				$url 	 = url('sites/default/files/'.$url_href);
-			}
-
-			$link    = 'open_identification/'.$record['info']['open_identification_id'].'';
-			$string .= '<a href="'.url($link).'">';
-			$string .= 	'<div class="open_identification" style="float:left;padding-bottom: 40px;padding-right:40px;">';
-			$string .= 		'<div class="open_identification_image" style="float:left;">';
-			$string .= 			'<img src="'.$url.'" widht="180" height="100" />';
-			$string .= 		'</div>';
-			$string .= 		'<div class="open_identification_info" style="float:left;">';
-			$string .= 			'<div class="info" style="padding-right:40px;">';	
-			$string .= 				'<span class="title">'.t('User').': </span>';	
-			$string .= 				'<span class="value">';					
-			$string .= 					$record['info']['fullname'];
-			$string .= 				'</span>';
-			$string .= 			'</div>';
-			$string .= 			'<div class="info" style="padding-right:40px;">';
-			$string .= 				'<span class="title">'.t('Modified on').': </span>';	
-			$string .= 				'<span class="value">';					
-			$string .= 					date("d.m.y H:i:s",strtotime($record['info']['modified_date']));
-			$string .= 				'</span>';	
-			$string .= 			'</div>';
-			$string .= 			'<div class="info" style="padding-right:40px;">';
-			$string .= 				'<span class="title">'.t('Type').': </span>';	
-			$string .= 				'<span class="value">';					
-			$string .= 					t($record['info']['type']);
-			$string .= 				'</span>';	
-			$string .= 			'</div>';
-		
-			if(isset($record['info']['organismgroup'])){
+//dpm($list);
+		foreach($list as $typerecordkey => $typerecord){
 			
-				$string .= 			'<div class="info">';
-				$string .= 				'<span class="title">'.t('Organismgroup').': </span>';	
-				$string .= 				'<span class="value">';					
-				$string .= 					t($record['info']['organismgroup']);
-				$string .= 				'</span>';	
-				$string .= 			'</div>';
-			}
-			else
-			{	$string .= 			'<div class="info">';
-				$string .= 				'<div class="title">&nbsp;</div>';	
-				$string .= 				'<div class="value">&nbsp;</div>';	
-				$string .= 			'</div>';
-			}
-			
+			$string .= '<div class="type"><div class="typetitle">'.t($typerecordkey).'</div><div class="typerecords">';
+			foreach($typerecord as $grouprecordkey => $grouprecord){
+				
+				$string .= '<div class="group"><div class="grouptitle">'.t($grouprecordkey).'</div><div class="grouprecords">';
+				foreach($grouprecord as $record){
 		
-			$test = "";
-			$string .= 		'</div>';
-			$string .= 	'</div>';
-			$string .= '</a>';
+					$medias = $record['medias'];
+					$media  = $medias[0];
+					$url    = "";
+					if($media['filemime'] == 'audio/mpeg'){
+						$url = url(drupal_get_path('module', 'gallery').'/images/music_icon.png');
+					}
+					elseif($media['filemime'] == 'video/mp4'){
+						$url = url(drupal_get_path('module', 'gallery').'/images/video_icon.png');
+					}
+					else{
+						$url_href  = file_uri_target($media['url']);
+						$url 	   = url('sites/default/files/'.$url_href);
+					}
+		
+					$link    = 'open_identification/'.$record['info']['open_identification_id'].'';
+					
+					$string .= 	'<div class="open_identification">';
+					$string .= '<a href="'.url($link).'">';
+					$string .= 		'<div class="open_identification_image">';
+					$string .= 			'<img src="'.$url.'"/>';
+					$string .= 		'</div>';
+					$string .= 		'<div class="open_identification_info">';
+					$string .= 			'<div class="info">';	
+					$string .= 				'<span class="title">'.t('User').': </span>';	
+					$string .= 				'<span class="value">';					
+					$string .= 					$record['info']['fullname'];
+					$string .= 				'</span>';
+					$string .= 			'</div>';
+					$string .= 			'<div class="info">';
+					$string .= 				'<span class="title">'.t('Modified on').': </span>';	
+					$string .= 				'<span class="value">';					
+					$string .= 					date("d.m.y H:i:s",strtotime($record['info']['modified_date']));
+					$string .= 				'</span>';	
+					$string .= 			'</div>';
+					$string .= 			'<div class="info">';
+					$string .= 				'<span class="title">'.t('Type').': </span>';	
+					$string .= 				'<span class="value">';					
+					$string .= 					t($record['info']['type']);
+					$string .= 				'</span>';	
+					$string .= 			'</div>';
+				
+					if(isset($record['info']['organismgroup'])){
+					
+						$string .= 			'<div class="info">';
+						$string .= 				'<span class="title">'.t('Organismgroup').': </span>';	
+						$string .= 				'<span class="value">';					
+						$string .= 					t($record['info']['organismgroup']);
+						$string .= 				'</span>';	
+						$string .= 			'</div>';
+					}
+					else
+					{	$string .= 			'<div class="info">';
+						$string .= 				'<div class="title">&nbsp;</div>';	
+						$string .= 				'<div class="value">&nbsp;</div>';	
+						$string .= 			'</div>';
+					}
+					
+				
+					$test = "";
+					$string .= 		'</div>';
+					$string .= 		'</a>';
+					$string .= 	'</div>';
+					
+				}
+				$string .= '</div></div>';
+			}
+			$string .= '</div></div>';
 		}
 }
 else{
