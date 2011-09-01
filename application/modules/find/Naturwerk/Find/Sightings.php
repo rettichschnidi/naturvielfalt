27849e77c723d6a07ca347c0c4832476bda98022
fabianvogler <fabianvogler@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-09-01 06:17:22 +0000

    Link on inventory name
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@733 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/find/Naturwerk/Find/Sightings.php
+++ application/modules/find/Naturwerk/Find/Sightings.php
@@ -15,10 +15,10 @@ class Sightings extends Inventories {
      */
     public function __construct(\Elastica_Index $index, Parameters $parameters) {
 
-        $this->addColumn('name_la', t('Latin name'), true, 'link', array($this, 'permission'));
-        $this->addColumn('name', t('German name'), true, 'link', array($this, 'permission'));
+        $this->addColumn('name_la', t('Latin name'));
+        $this->addColumn('name', t('German name'));
         $this->addColumn('user', t('User'));
-        $this->addColumn('inventory', t('Inventory'));
+        $this->addColumn('inventory', t('Inventory'), true, 'link', array($this, 'permission'));
         $this->addColumn('town', t('Town name'));
         $this->addColumn('canton', t('Canton'));
         $this->addColumn('date', t('Observation date'), true, 'date');
