8d4a4eac6a7018cbf2b2721e060f7d6c2df281fd
deyf <deyf@035473d3-0685-4573-a6ed-08f8c3112c85>
2011-10-04 07:55:34 +0000

    replaced ajax request
    git-svn-id: https://svn.origo.ethz.ch/swissmon/swissmon@823 035473d3-0685-4573-a6ed-08f8c3112c85

--- application/modules/area/js/area-edit.js
+++ application/modules/area/js/area-edit.js
@@ -8,10 +8,24 @@ var editIcon;
 var applyButton;
 var cancelButton;
 
-function enable_map_editing(map, overlays) {
+function enable_map_editing() {
 	if (jQuery("#edit-map-button").length) {
+		
+		map = createGoogleMaps("map_canvas");
+		
 		//editable overlay
-		currentOverlays = overlays;
+		currentOverlays = new GeometryOverlays(map);
+		
+		var coords = jQuery("#area-coords-input").val().replace("&quot;", '"');
+		var coordsArray = JSON.parse(jQuery("#area-coords-input").val());
+		    
+		area = new Object();
+		area.type = jQuery("#area-type-input").val();
+		var coords = jQuery("#area-coords-input").val().replace("&quot;", '"');
+		area.area_points = JSON.parse(jQuery("#area-coords-input").val());
+		a = new Array();
+		a[0] = area;
+		currentOverlays.addOverlaysJson(a);
 		
 		//init controls
 		controls = jQuery('<div style="margin: 5px;"></div>');
@@ -50,6 +64,33 @@ function enable_map_editing(map, overlays) {
 	    map.controls[google.maps.ControlPosition.TOP_LEFT].push(controls.get(0));
 	    activeEditingTool.setMap(map);
 	}
+};
+
+
+/**
+ * Creates the google maps object and attaches it to the element with the id
+ * 'map_canvas'. Returns a google.maps.Map
+ */
+function createGoogleMaps (map_id) {
+  var mapcenter = [46.77373, 8.25073];
+  var canvas = jQuery('#'+ map_id);
+  if(!canvas.size())
+    return false;
+  zoom = parseInt(canvas.data('zoom'));
+  mapcenter = canvas.data('center').split(',');
+  center = new google.maps.LatLng(parseFloat(mapcenter[0]), parseFloat(mapcenter[1]));
+  var mapsOptions = {
+      zoom : zoom,
+      center : center,
+      mapTypeId : google.maps.MapTypeId.ROADMAP,
+      maxZoom: 18,
+      minZoom: 5,
+      scrollwheel: true
+    };
+  
+  var map = new google.maps.Map(document.getElementById(map_id), mapsOptions);
+
+  return map;
 };
 
 function edit_click() {
@@ -108,3 +149,11 @@ function dump(obj) {
 
     console.debug(out);
 };
+
+/*load the area*/
+var areaselect = null;
+jQuery(document).ready(function() {
+  if(jQuery('#map_canvas').size()) {
+	  enable_map_editing();
+  }
+});
