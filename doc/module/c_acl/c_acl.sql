c30d748c2fb2c975d560f8c287b3a28455717a35
Simon Maurer <moiri@gmail.com>
2012-02-17 10:53:50 +0100

    seperate acl and group module

---
+++ doc/module/c_acl/c_acl.sql
@@ -0,0 +1,68 @@
+
+/* Drop Tables */
+
+DROP TABLE IF EXISTS acl_link;
+DROP TABLE IF EXISTS acl;
+
+
+
+
+/* Create Tables */
+
+-- ACL items
+CREATE TABLE acl
+(
+	-- Primary key for swissmon acl items
+	id bigserial NOT NULL UNIQUE,
+	-- desccription of acl item
+	description text,
+	-- foreign key for swissmon user id who created acl item
+	users_id bigint DEFAULT 0,
+	PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+
+-- Links acl items to users and groups
+CREATE TABLE acl_link
+(
+	-- primary key of linking table
+	id bigserial NOT NULL,
+	-- access level
+	level smallint,
+	-- Foreign key of acl id
+	acl_id bigint DEFAULT 0,
+	-- foreign key for swissmon user id
+	users_id bigint DEFAULT 0,
+	-- Foreign key of swissmon sgroup
+	sgroup_id bigint DEFAULT 0,
+	PRIMARY KEY (id)
+) WITHOUT OIDS;
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE acl_link
+	ADD FOREIGN KEY (acl_id)
+	REFERENCES acl (id)
+	ON UPDATE RESTRICT
+	ON DELETE RESTRICT
+;
+
+
+
+/* Comments */
+
+COMMENT ON TABLE acl IS 'ACL items';
+COMMENT ON COLUMN acl.id IS 'Primary key for swissmon acl items';
+COMMENT ON COLUMN acl.description IS 'desccription of acl item';
+COMMENT ON COLUMN acl.users_id IS 'foreign key for swissmon user id who created acl item';
+COMMENT ON TABLE acl_link IS 'Links acl items to users and groups';
+COMMENT ON COLUMN acl_link.id IS 'primary key of linking table';
+COMMENT ON COLUMN acl_link.level IS 'access level';
+COMMENT ON COLUMN acl_link.acl_id IS 'Foreign key of acl id';
+COMMENT ON COLUMN acl_link.users_id IS 'foreign key for swissmon user id';
+COMMENT ON COLUMN acl_link.sgroup_id IS 'Foreign key of swissmon sgroup';
+
+
+
--- doc/module/sgroup/sgroup.sql
+++ doc/module/sgroup/sgroup.sql
@@ -1,8 +1,6 @@
 
 /* Drop Tables */
 
-DROP TABLE IF EXISTS sgroup_acl_link;
-DROP TABLE IF EXISTS sgroup_acl;
 DROP TABLE IF EXISTS public.sgroup_users;
 DROP TABLE IF EXISTS public.sgroup;
 
@@ -10,36 +8,6 @@ DROP TABLE IF EXISTS public.sgroup;
 
 
 /* Create Tables */
-
--- ACL items
-CREATE TABLE sgroup_acl
-(
-	-- Primary key for swissmon acl items
-	id bigserial NOT NULL UNIQUE,
-	-- desccription of acl item
-	description text,
-	-- foreign key for swissmon user id who created acl item
-	users_id bigint DEFAULT 0,
-	PRIMARY KEY (id)
-) WITHOUT OIDS;
-
-
--- Links acl items to users and groups
-CREATE TABLE sgroup_acl_link
-(
-	-- access level
-	level smallint,
-	-- foreign key for swissmon user id
-	users_id bigint DEFAULT 0,
-	-- Foreign key of acl id
-	acl_id bigint DEFAULT 0,
-	-- Foreign key of swissmon sgroup
-	sgroup_id bigint DEFAULT 0,
-	-- primary key of linking table
-	id bigserial NOT NULL,
-	PRIMARY KEY (id)
-) WITHOUT OIDS;
-
 
 -- Holds user groups
 CREATE TABLE public.sgroup
@@ -67,22 +35,6 @@ CREATE TABLE public.sgroup_users
 
 /* Create Foreign Keys */
 
-ALTER TABLE sgroup_acl_link
-	ADD FOREIGN KEY (acl_id)
-	REFERENCES sgroup_acl (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
-ALTER TABLE sgroup_acl_link
-	ADD FOREIGN KEY (sgroup_id)
-	REFERENCES public.sgroup (id)
-	ON UPDATE RESTRICT
-	ON DELETE RESTRICT
-;
-
-
 ALTER TABLE public.sgroup_users
 	ADD FOREIGN KEY (sgroup_id)
 	REFERENCES public.sgroup (id)
@@ -94,16 +46,6 @@ ALTER TABLE public.sgroup_users
 
 /* Comments */
 
-COMMENT ON TABLE sgroup_acl IS 'ACL items';
-COMMENT ON COLUMN sgroup_acl.id IS 'Primary key for swissmon acl items';
-COMMENT ON COLUMN sgroup_acl.description IS 'desccription of acl item';
-COMMENT ON COLUMN sgroup_acl.users_id IS 'foreign key for swissmon user id who created acl item';
-COMMENT ON TABLE sgroup_acl_link IS 'Links acl items to users and groups';
-COMMENT ON COLUMN sgroup_acl_link.level IS 'access level';
-COMMENT ON COLUMN sgroup_acl_link.users_id IS 'foreign key for swissmon user id';
-COMMENT ON COLUMN sgroup_acl_link.acl_id IS 'Foreign key of acl id';
-COMMENT ON COLUMN sgroup_acl_link.sgroup_id IS 'Foreign key of swissmon sgroup';
-COMMENT ON COLUMN sgroup_acl_link.id IS 'primary key of linking table';
 COMMENT ON TABLE public.sgroup IS 'Holds user groups';
 COMMENT ON COLUMN public.sgroup.id IS 'Primary key for swissmon groups';
 COMMENT ON COLUMN public.sgroup.name IS 'The name of the swissmon group';
