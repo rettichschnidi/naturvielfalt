27cc03ec113c64087afe88e86f0fce303d019460
Reto Schneider <github@reto-schneider.ch>
2012-03-15 16:29:05 +0100

    Fixes

--- webroot_drupal/sites/all/modules/area/area.new.tpl.php
+++ webroot_drupal/sites/all/modules/area/area.new.tpl.php
@@ -5,7 +5,8 @@ drupal_add_library('system', 'ui.autocomplete');
 // include JavaScripts
 $areaJsPath = base_path() . drupal_get_path('module', 'area') . '/js/';
 $areaCSSPath = base_path() . drupal_get_path('module', 'area') . '/css/';
-$inventoryJsPath = base_path() . drupal_get_path('module', 'inventory') . '/js/';
+$inventoryJsPath = base_path() . drupal_get_path('module', 'inventory')
+		. '/js/';
 
 $jsLibs = array(
 		'http://maps.google.com/maps/api/js?sensor=false&libraries=geometry,places',
