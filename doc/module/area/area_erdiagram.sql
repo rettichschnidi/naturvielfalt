1d8c2dff99a7dbc59f0d472ff243e16a39085e66
Reto Schneider <github@reto-schneider.ch>
2012-03-21 08:34:00 +0100

    Update area db-schema

--- doc/module/area/area_erdiagram.sql
+++ doc/module/area/area_erdiagram.sql
@@ -1,7 +1,6 @@
 
 /* Drop Indexes */
 
-DROP INDEX IF EXISTS area_hidden;
 DROP INDEX IF EXISTS fki_area_id;
 
 
@@ -12,7 +11,6 @@ DROP TABLE IF EXISTS public.area_file_managed;
 DROP TABLE IF EXISTS public.area_habitat;
 DROP TABLE IF EXISTS public.area_parcel;
 DROP TABLE IF EXISTS public.area;
-DROP TABLE IF EXISTS public.area_type;
 
 
 
@@ -23,29 +21,28 @@ CREATE TABLE public.area
 (
 	-- Primary Key
 	id serial NOT NULL,
+	-- Primary key for swissmon acl items
+	acl_id int NOT NULL,
 	-- Name, Flurname
-	field_name text NOT NULL,
-	-- Meter √É¬ºber Meer
+	name text NOT NULL,
+	-- Meter über Meer
 	altitude int DEFAULT 0 NOT NULL,
-	-- Fl√É¬§che in (m2)
+	-- Fläche in m²
 	surface_area bigint DEFAULT 0 NOT NULL,
-	locality text NOT NULL,
 	zip text NOT NULL,
 	township text NOT NULL,
 	canton text NOT NULL,
 	country text NOT NULL,
-	-- Kommentar
-	comment text,
-	owner_id int,
-	create_time timestamp with time zone,
-	modify_time timestamp with time zone,
+	-- Kommentartext
+	commenttext text,
+	create_time timestamp,
+	modify_time timestamp,
 	-- Schutzziel
 	protection_target text,
 	-- Schutzmassnahmen
 	safety_precautions text,
 	-- Pflege- und Gestaltungsmassnahmen
 	tending_strategies text,
-	hidden boolean,
 	geom geometry,
 	PRIMARY KEY (id)
 ) WITHOUT OIDS;
@@ -84,16 +81,6 @@ CREATE TABLE public.area_parcel
 ) WITHOUT OIDS;
 
 
-CREATE TABLE public.area_type
-(
-	-- PK
-	id serial NOT NULL,
-	-- Description
-	description text NOT NULL,
-	PRIMARY KEY (id)
-) WITHOUT OIDS;
-
-
 
 /* Create Foreign Keys */
 
@@ -124,7 +111,6 @@ ALTER TABLE public.area_parcel
 
 /* Create Indexes */
 
-CREATE INDEX area_hidden ON public.area USING BTREE (hidden);
 CREATE INDEX fki_area_id ON public.area_parcel USING BTREE (area_id);
 
 
@@ -132,10 +118,11 @@ CREATE INDEX fki_area_id ON public.area_parcel USING BTREE (area_id);
 /* Comments */
 
 COMMENT ON COLUMN public.area.id IS 'Primary Key';
-COMMENT ON COLUMN public.area.field_name IS 'Name, Flurname';
-COMMENT ON COLUMN public.area.altitude IS 'Meter √É¬ºber Meer';
-COMMENT ON COLUMN public.area.surface_area IS 'Fl√É¬§che in (m2)';
-COMMENT ON COLUMN public.area.comment IS 'Kommentar';
+COMMENT ON COLUMN public.area.acl_id IS 'Primary key for swissmon acl items';
+COMMENT ON COLUMN public.area.name IS 'Name, Flurname';
+COMMENT ON COLUMN public.area.altitude IS 'Meter über Meer';
+COMMENT ON COLUMN public.area.surface_area IS 'Fläche in m²';
+COMMENT ON COLUMN public.area.commenttext IS 'Kommentartext';
 COMMENT ON COLUMN public.area.protection_target IS 'Schutzziel';
 COMMENT ON COLUMN public.area.safety_precautions IS 'Schutzmassnahmen';
 COMMENT ON COLUMN public.area.tending_strategies IS 'Pflege- und Gestaltungsmassnahmen';
@@ -145,8 +132,6 @@ COMMENT ON COLUMN public.area_habitat.id IS 'PK';
 COMMENT ON COLUMN public.area_habitat.area_id IS 'FK to area';
 COMMENT ON COLUMN public.area_habitat.habitat_id IS 'PK';
 COMMENT ON COLUMN public.area_parcel.area_id IS 'Primary Key';
-COMMENT ON COLUMN public.area_type.id IS 'PK';
-COMMENT ON COLUMN public.area_type.description IS 'Description';
 
 
 
