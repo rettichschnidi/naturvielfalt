ea9ae3b4bafbaa58dc7b2e128126f8afafa029be
Ramon Gamma <ramon@gamma.li>
2012-03-21 10:26:10 +0100

    Add & beginning with new module observation

---
+++ webroot_drupal/sites/all/modules/observation/js/util.js
@@ -0,0 +1,86 @@
+// FIXME
+/**
+ * 
+ * FALSCHE BESCHREIBUNG???
+ * 
+ * 
+ * This function creates a autocomplete field, in which you must select a value
+ * from the dropdown.
+ * 
+ * @param textField
+ *            The text field, where the user can enter the text.
+ * @param idField
+ *            A hidden input field, where the id will be saved.
+ * @param source
+ *            The source, where we can get the items for the autocomplete field.
+ *            The source must return a JSON in the form [{id, label, ....}]
+ * @param onSelect
+ *            This function will be executed, when an entry has been selected.
+ * @return
+ */
+// FIXME
+
+
+var util = {};
+
+/**
+ * 
+ * @param input_id
+ * @param search_button_id
+ * @param map
+ */
+util.geocode = function(input_id, search_button_id, map) {
+	// FIXME
+	me = this;
+	var geocoder = new google.maps.Geocoder();
+
+	jQuery('#' + input_id)
+			.geo_autocomplete(
+					{
+						geocoder_region : 'Schweiz',
+						geocoder_types : 'natural_feature,street_address,route,intersection,locality,political,sublocality,neighborhood,country',
+						maptype : 'roadmap',
+						mapwidth : 200,
+						select : function(_event, _ui) {
+							if (_ui.item.viewport)
+								map.fitBounds(_ui.item.viewport);
+						}
+					});
+
+	// TODO funktion auch ausführen, wenn die enter taste betätigt wurde...
+	jQuery('#' + search_button_id).click(function() {
+		console.info('geocode');
+		var term = jQuery('#' + input_id).val().trim();
+		if (term.length > 3) {
+			var geo_opts = {
+				address : term,
+				language : 'de',
+				region : 'CH'
+			};
+			console.info(geo_opts);
+			if (geocoder) {
+				geocoder.geocode(geo_opts, function(results, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						console.info(results);
+						map.fitBounds(results[0].geometry.viewport);
+
+					} else {
+						// TODO: Change alert!
+						alert("Geocoder failed due to: " + status);
+					}
+				});
+			}
+		}
+
+	});
+};
+
+/**
+ * 
+ * @param a
+ * @param i
+ * @param m
+ */
+jQuery.expr[':'].textEquals = function(a, i, m) {
+	return jQuery(a).text().match("^" + m[3] + "$");
+};
