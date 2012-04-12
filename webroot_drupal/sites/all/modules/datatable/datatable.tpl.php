899593f6edacb5baf4cdba2a9456a7410869d406
Reto Schneider <github@reto-schneider.ch>
2012-04-12 13:17:44 +0200

    Make width for each table header required.

--- webroot_drupal/sites/all/modules/datatable/datatable.tpl.php
+++ webroot_drupal/sites/all/modules/datatable/datatable.tpl.php
@@ -26,46 +26,20 @@ drupal_add_js(drupal_get_path('module', 'datatable') . '/js/flexigrid.js');
 drupal_add_js(
 	drupal_get_path('module', 'datatable') . '/js/lib/jquery.cookie.js');
 
-if (isset($options['tableWidth'])
-		) {
+/**
+ * Figure out width/height of table or set default values
+ */
+if (isset($options['tableWidth']) && is_int($options['tableWidth'])) {
 	$tableWidth = $options['tableWidth'];
 } else {
-	$tableWidth = 800;
+	$tableWidth = 950;
 }
-
-if (isset($options['tableHeight']) && $options['tableHeight'] > 0) {
+if (isset($options['tableHeight']) && is_int($options['tableHeight'])
+		&& $options['tableHeight'] > 0) {
 	$tableHeight = $options['tableHeight'];
 } else {
 	$tableHeight = 200;
 }
-
-/**
- * Calculate the width of each column
- */
-$allHeaders = "";
-$allWidths = 0;
-foreach ($header as $head) {
-	// jump over invisible headers
-	$visible = isset($head['hide']) ? !$head['hide'] : true;
-	if ($visible) {
-		if (isset($head['width']) && is_int($head['width'])) {
-			$allWidths = $allWidths + $head['width'];
-		} else {
-			$allHeaders .= $head['name'];
-		}
-	}
-}
-
-/**
- * Does something as well
- */
-$countHeadChars = strlen($allHeaders);
-if (is_int($tableWidth) && $countHeadChars > 0) {
-	$charTableSize = ($tableWidth - $allWidths - 62) / $countHeadChars;
-} else {
-	$charTableSize = 0;
-}
-$colsized = (7 > $charTableSize) ? 8 : $charTableSize;
 
 /**
  * Render the headers and columns
@@ -77,21 +51,23 @@ if ($header) {
 	$sortOrder = "asc";
 
 	foreach ($header as $head) {
-		$aoColumns .= "{ display: '" . $head['name'] . "', name : '"
-				. $head['dbfield'] . "'";
-		// 		if($head['hide'] == true) $aoColumns .= ", 'bVisible' : false";
-		// 		if($head['noSort'] == true) $aoColumns .= ", 'bSortable' : false";
-		(isset($head['noSort']) && $head['noSort'] == true) ? $aoColumns .= ",sortable : false"
-				: $aoColumns .= ",sortable : true";
-		(isset($head['width']) && $head['width']) ? $aoColumns .= ",  width : "
-						. $head['width']
-				: $aoColumns .= ",  width : "
-						. (strlen($head['name']) * $colsized);
+		$aoColumns .= "{ display: '" . $head['name'] . "'";
+		$aoColumns .= ", name : '" . $head['dbfield'] . "'";
+		if (isset($head['noSort']) && $head['noSort'] == true) {
+			$aoColumns .= ", sortable : false";
+		} else {
+			$aoColumns .= ", sortable : true";
+		}
+		assert($head['width']);
+		$aoColumns .= ", width : '" . $head['width'] . "'";
+
 		(isset($head['align']) && $head['align']) ? $aoColumns .= ",  align : '"
-						. $head['width'] . "'"
+						. 9333 . "'"
 				: $aoColumns .= ",  align : 'left'";
+
 		if (isset($head['hide']) && $head['hide'] == true)
 			$aoColumns .= ", hide : true";
+
 		if (isset($options['rowClick']))
 			$aoColumns .= ", process : " . $options['rowClick'];
 		$aoColumns .= "},";
@@ -106,50 +82,13 @@ if ($header) {
 	$aoColumns = substr_replace($aoColumns, "", -1);
 	$aoColumns .= "],";
 }
-
-/**
- * setup and render the table
- */
-/**$table[$id_table] = array(
-        '#theme' => 'table',
-        '#header' => $headers,
-        '#rows' => $rows,
-        '#sticky' => false,
-        '#attributes' => array(
-                'id' => $id_table,
-                'width' => "100%"
-        ),
-    );
-print drupal_render($table);
- */
-/**
- * set the height of the table
- */
-// if ($len == NULL) {
-// 	$len = 250;
-// }
-
-/**
- * progress the additional options for the table
- */
-
-/**$additional_options = ','.$aoColumns.'';
-if (is_array($options)) {
-    foreach ($options as $name => $value) {
-        $additional_options .= ', "' . $name . '": ' . $value.'';
-    }
-}
- */
 ?>
 
 <table id="<?php echo $id_table; ?>" class="<?php echo $id_table; ?>"></table>
 <script type="text/javascript" charset="utf-8">
 <!--
 
-//alert('1');
-
 jQuery(document).ready(function() {
-//$=jQuery;
 
 jQuery("#<?php echo $id_table; ?>").flexigrid
 (
@@ -162,7 +101,8 @@ sortname: "<?php echo $sortField; ?>",
 sortorder: "<?php echo $sortOrder; ?>",
 usepager: true,
 <?php if ($title)
-	echo "title: '" . $title . "'," ?>
+	echo "title: '" . $title . "',"
+?>
 useRp: true,
 rp: 15,
 showTableToggleBtn: true,
