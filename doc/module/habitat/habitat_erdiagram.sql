612853e96fa24ee3bca2f8648d2f515810ed78bc
Reto Schneider <github@reto-schneider.ch>
2012-04-23 16:35:14 +0200

    Add latin name.

--- doc/module/habitat/habitat_erdiagram.sql
+++ doc/module/habitat/habitat_erdiagram.sql
@@ -22,8 +22,15 @@ CREATE TABLE public.habitat
 	label text NOT NULL UNIQUE,
 	-- name
 	name text NOT NULL,
+	name_scientific text,
 	-- Bemerkungen zum Lebensraumtyp
 	notes text,
+	-- Not used so far - just in case we ever need this data again.
+	name_de text,
+	-- Not used so far - just in case we ever need this data again.
+	name_it text,
+	-- Not used so far - just in case we ever need this data again.
+	name_fr text,
 	PRIMARY KEY (id)
 ) WITHOUT OIDS;
 
@@ -35,6 +42,9 @@ COMMENT ON COLUMN public.habitat.id IS 'PK';
 COMMENT ON COLUMN public.habitat.label IS 'Label inkl. Hierarchie';
 COMMENT ON COLUMN public.habitat.name IS 'name';
 COMMENT ON COLUMN public.habitat.notes IS 'Bemerkungen zum Lebensraumtyp';
+COMMENT ON COLUMN public.habitat.name_de IS 'Not used so far - just in case we ever need this data again.';
+COMMENT ON COLUMN public.habitat.name_it IS 'Not used so far - just in case we ever need this data again.';
+COMMENT ON COLUMN public.habitat.name_fr IS 'Not used so far - just in case we ever need this data again.';
 
 
 
