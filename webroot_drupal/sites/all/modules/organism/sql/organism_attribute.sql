a38975ebc3cdf1a5297417d532b9fc8afa1fcf75
Reto Schneider <github@reto-schneider.ch>
2012-05-02 15:22:44 +0200

    Remove drupal_ prefix in organism sql files.

--- webroot_drupal/sites/all/modules/organism/sql/organism_attribute.sql
+++ webroot_drupal/sites/all/modules/organism/sql/organism_attribute.sql
@@ -11,23 +11,23 @@ SET client_min_messages = warning;
 SET search_path = public, pg_catalog;
 
 --
--- Name: drupal_organism_attribute_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Name: {organism_attribute_id_seq}; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('drupal_organism_attribute_id_seq', 7, true);
+SELECT pg_catalog.setval('{organism_attribute_id_seq}', 7, true);
 
 
 --
--- Data for Name: drupal_organism_attribute; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: {organism_attribute}; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (1, 'n', 'NUESP', 'Number which got assigned to this organism by the CSCF.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (2, 'n', 'CRSF number', 'Number which got assigned to this organism by the CRSF.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (3, 't', 'Author', 'Name of the scientist who classified this organism.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (4, 'b', 'Xenophyte', 'Wether this organism is imported or not.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (5, 't', 'Flora Helvetica', 'Number which got assigned to this organism by the Flora Helvetica.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (6, 'n', 'Fungus number', 'Number which got assigned to this organism by the WSL.');
-INSERT INTO drupal_organism_attribute (id, valuetype, name, comment) VALUES (7, 't', 'First find', 'The year of the first documented sight of this organism.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (1, 'n', 'NUESP', 'Number which got assigned to this organism by the CSCF.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (2, 'n', 'CRSF number', 'Number which got assigned to this organism by the CRSF.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (3, 't', 'Author', 'Name of the scientist who classified this organism.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (4, 'b', 'Xenophyte', 'Wether this organism is imported or not.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (5, 't', 'Flora Helvetica', 'Number which got assigned to this organism by the Flora Helvetica.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (6, 'n', 'Fungus number', 'Number which got assigned to this organism by the WSL.');
+INSERT INTO {organism_attribute} (id, valuetype, name, comment) VALUES (7, 't', 'First find', 'The year of the first documented sight of this organism.');
 
 
 --
