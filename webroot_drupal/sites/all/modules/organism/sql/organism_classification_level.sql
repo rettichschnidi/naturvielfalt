da7a7e29b5b38537128e38e59091a92821a7b4ca
Reto Schneider <github@reto-schneider.ch>
2012-05-15 12:11:41 +0200

    Revert new sql files which where wrongly sorted.

--- webroot_drupal/sites/all/modules/organism/sql/organism_classification_level.sql
+++ webroot_drupal/sites/all/modules/organism/sql/organism_classification_level.sql
@@ -14,26 +14,31 @@ SET search_path = public, pg_catalog;
 -- Name: {organism_classification_level_id_seq}; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('{organism_classification_level_id_seq}', 13, true);
+SELECT pg_catalog.setval('{organism_classification_level_id_seq}', 18, true);
 
 
 --
 -- Data for Name: {organism_classification_level}; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (1, 1, 1, 1, 12, 'CSCF');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (2, 1, 1, 2, 11, 'class');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (3, 2, 1, 3, 10, 'order');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (4, 3, 1, 4, 9, 'family');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (5, 4, 1, 5, 8, 'genus');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (6, 5, 1, 6, 7, 'subgenus');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (7, 7, 7, 1, 6, 'CRSF');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (8, 7, 7, 2, 5, 'family');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (9, 8, 7, 3, 4, 'genus');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (10, 10, 10, 1, 4, 'Fungus');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (11, 10, 10, 2, 3, 'class');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (12, 12, 12, 1, 4, 'Swisslichens');
-INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (13, 12, 12, 2, 3, 'genus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (1, 1, 1, 1, 16, 'CSCF');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (2, 1, 1, 2, 15, 'class');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (3, 2, 1, 3, 14, 'order');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (4, 3, 1, 4, 13, 'family');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (5, 4, 1, 5, 12, 'genus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (6, 5, 1, 6, 11, 'subgenus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (7, 6, 1, 7, 10, 'species');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (8, 7, 1, 8, 9, 'subspecies');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (9, 9, 9, 1, 10, 'CRSF');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (10, 9, 9, 2, 9, 'family');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (11, 10, 9, 3, 8, 'genus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (12, 11, 9, 4, 7, 'species');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (13, 12, 9, 5, 6, 'subspecies');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (14, 14, 14, 1, 4, 'Fungus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (15, 14, 14, 2, 3, 'class');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (16, 16, 16, 1, 6, 'Swisslichens');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (17, 16, 16, 2, 5, 'genus');
+INSERT INTO {organism_classification_level} (id, parent_id, prime_father_id, left_value, right_value, name) VALUES (18, 17, 16, 3, 4, 'species');
 
 
 --
