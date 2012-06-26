e244687ede48b02bd65b2b21b0b6daed8c8f3b10
Reto Schneider <github@reto-schneider.ch>
2012-06-26 16:49:10 +0200

    Restore the intended functionality:  - Selecting a speciesgroup limits the autocomplete results  - Re-create an speciesgroup which contains all the other ones  - Using recursive SQL (with recursive...) instead of PHP garbage  - Fix spelling error

--- webroot_drupal/sites/all/modules/organism/sql/organism_artgroup.sql
+++ webroot_drupal/sites/all/modules/organism/sql/organism_artgroup.sql
@@ -14,39 +14,40 @@ SET search_path = public, pg_catalog;
 -- Name: {organism_artgroup_id_seq}; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('{organism_artgroup_id_seq}', 26, true);
+SELECT pg_catalog.setval('{organism_artgroup_id_seq}', 27, true);
 
 
 --
 -- Data for Name: {organism_artgroup}; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (2, 'Amphibien', 2, 1);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (11, 'Eintagsfliegen', 11, 2);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (1, 'Fische', 1, 3);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (24, 'Flora', 24, 4);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (26, 'Fungus', 26, 5);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (14, 'Hautflügler', 14, 6);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (9, 'Heuschrecken', 9, 7);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (7, 'Käfer', 7, 8);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (20, 'Köcherfliegen', 20, 9);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (21, 'Krebse', 21, 10);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (8, 'Laufkäfer', 8, 11);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (17, 'Libellen', 17, 12);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (25, 'Lichens', 25, 13);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (5, 'Mollusken', 5, 14);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (15, 'Nachtfalter', 15, 15);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (18, 'Netzflügler', 18, 16);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (23, 'Reptilien', 23, 17);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (22, 'Säugetiere', 22, 18);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (3, 'Spinnen', 3, 19);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (19, 'Steinfliegen', 19, 20);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (16, 'Tagfalter', 16, 21);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (6, 'Tausendfüsser', 6, 22);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (4, 'Vögel', 4, 23);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (13, 'Wanzen', 13, 24);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (12, 'Zikaden', 12, 25);
-INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (10, 'Zweiflügler', 10, 26);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (1, 'Alle', NULL, 0);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (8, 'Laufkäfer', 7, 11);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (2, 'Amphibien', 1, 0);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (14, 'Hautflügler', 1, 6);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (11, 'Eintagsfliegen', 1, 2);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (24, 'Flora', 1, 4);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (26, 'Fungus', 1, 5);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (9, 'Heuschrecken', 1, 7);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (7, 'Käfer', 1, 8);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (20, 'Köcherfliegen', 1, 9);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (21, 'Krebse', 1, 10);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (17, 'Libellen', 1, 12);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (25, 'Lichens', 1, 13);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (5, 'Mollusken', 1, 14);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (15, 'Nachtfalter', 1, 15);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (18, 'Netzflügler', 1, 16);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (23, 'Reptilien', 1, 17);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (22, 'Säugetiere', 1, 18);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (3, 'Spinnen', 1, 19);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (19, 'Steinfliegen', 1, 20);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (16, 'Tagfalter', 1, 21);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (6, 'Tausendfüsser', 1, 22);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (4, 'Vögel', 1, 23);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (13, 'Wanzen', 1, 24);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (12, 'Zikaden', 1, 25);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (10, 'Zweiflügler', 1, 26);
+INSERT INTO {organism_artgroup} (id, name, parent, pos) VALUES (27, 'Fische', 1, 3);
 
 
 --
