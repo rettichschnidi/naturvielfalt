e86f4afe80bf197f437a15960defba261b80fa4d
Reto Schneider <github@reto-schneider.ch>
2012-07-04 12:28:49 +0200

    Remove dead code.

--- webroot_drupal/sites/all/modules/observation/js/observation_autocomplete.js
+++ webroot_drupal/sites/all/modules/observation/js/observation_autocomplete.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function() {
-
 		
 $ = jQuery;
 var autocomplete = {};
@@ -26,10 +25,10 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 				artgroup: $('#artgroup').val(),
 				term: actualElement.val()
 			},
-			// success : response,
+			// success : response
 			success: function (data) {
 				if(data == null || data.length == 0) {
-					//change visual indicator to notfound and hide menu again
+					// change visual indicator to notfound and hide menu again
 					actualElement.removeClass("searching");
 					actualElement.addClass("notfound");
 					$('.ui-autocomplete').hide();
@@ -41,10 +40,7 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 			}
 		});
 	},
-//	focus: function (event, ui) {
-//		inventory.scrollToAutocompleteItem(this);
-//		return false;
-//	},
+	
 	select: function (event, ui) {
 		$(this).val(ui.item.label || ui.item.label_latin_short);
 		lan = ui.item.langfounded;
@@ -58,7 +54,6 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 			$( "#species_autocomplete" ).html(ui.item.label);
 		}else{
 			$( "#organismn_id" ).val(ui.item.id);
-//			$( "#organismn_autocomplete" ).val(ui.item.label_latin);
 			$( "#species_autocomplete" ).html(ui.item.label_latin);
 		}
 		
@@ -69,48 +64,30 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 			observation.showDetMethod(ui.item.det_methods[i]);
 		}
 		
-//		alert('lang:' + ui.item.langfounded + ' latin:' + ui.item.latinfounded);
-//		$(this).closest('tr').find('td > em').html(ui.item.label_latin);
-//		inventory.enableDisable($(this).closest('tr'));
 		if(event.keyCode == 9 || event.keyCode == 13) { // TAB, ENTER
-//			$( "#date" ).focus();
 			$( "#date" ).focus();
-//			event.preventDefault();
 		}
-//		if($(this).closest('tr:last-child').size()) inventory.duplicateRow($(this).closest('tr'));
-//		inventory.getImage($(this).closest('tr'));
 		return false;
 	}
 }).keyup(function () {
-	//monitor field and remove class 'notfound' if its length is less than 2
+	// monitor field and remove class 'notfound' if its length is less than 2
 	if($(this).val().length < 2) {
 		$(this).removeClass('notfound');
 		$( "#organismn_id" ).val();
 		$( "#species_autocomplete" ).html('');
-//		$(this).closest('td').find('input.organism_id').val('');
-//		$(this).closest('tr').find('td > em').html('');
-//		$(this).closest('tr').find('.image').html('');
-//		inventory.enableDisable($(this).closest('tr'));
 	}
 }).each(function () {
 	$(this).data('autocomplete')._renderItem = function (ul, item) {
 		term = this.term;
-//		term = this.term.replace(/[aou]/, function (m) {
-//			// to find with term 'wasser' -> 'wasser' and 'gewasesser'
-//			var hash = {
-//				'a': '(ä|a)',
-//				'o': '(ö|o)',
-//				'u': '(ä|u)'
-//			};
-//			return hash[m];
-//		});
 		// highlighting of matches
 		spacer = '';
 		var label = item.label;
 		var label_latin = item.label_latin;
 		var re = new RegExp($.trim(term), 'ig');
 		item.langfounded = false;
-		if(label.match(re)) item.langfounded = true;
+		if(label.match(re)) {
+			item.langfounded = true;
+		}
 		label = label.replace(re, '<span class="ui-state-highlight">$&</span>');
 		
 		// mark the search result in the sc name
@@ -119,7 +96,9 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 		label_latin_split = label_latin.split(' ');
 		
 		re = new RegExp(term[0], 'ig');
-		if(label_latin_split[0].match(re)) item.latinfounded = true;
+		if(label_latin_split[0].match(re)) {
+			item.latinfounded = true;
+		}
 		label_latin_split[0] = label_latin_split[0].replace(re, '<span class="ui-state-highlight">$&</span>');
 		
 		if(label_latin_split.length > 1 && term.length > 1){
@@ -135,21 +114,9 @@ jQuery( "#organismn_autocomplete" ).autocomplete({
 		}
 		label_latin = label_latin_split.join(" ");
 		
-//		
-//		var old_label = item.old_label;
-//		var old_label_latin = item.old_label_latin;
-//		var re = new RegExp($.trim(term), 'ig');
-//		if(old_label) old_label = old_label.replace(re, '<span class="ui-state-highlight">$&</span>');
-//		else label = label.replace(re, '<span class="ui-state-highlight">$&</span>');
-//		term = $.trim(term).split(' ');
-//		while(term.length) {
-//			re = new RegExp(term.pop(), 'ig');
-//			if(old_label_latin) old_label = old_label.replace(re, '<span class="ui-state-highlight">$&</span>');
-//			else label_latin = label_latin.replace(re, '<span class="ui-state-highlight">$&</span>');
-//		}
-//		var old = '';
-//		if(old_label || old_label_latin) old = '<small>' + old_label + '<em>' + old_label_latin + '</em></small>';
-		if(label_latin != '' && label != '') spacer = '<br>';
+		if(label_latin != '' && label != '')  {
+			spacer = '<br>';
+		}
 		return $('<li></li>').data('item.autocomplete', item).append('<a class="organism_autocomplete_list">' + label + spacer +'<em class="organism_autocomplete_list_s">' + label_latin + '</em></a>').appendTo(ul);
 	};
 });
