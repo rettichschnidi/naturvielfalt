48eddf7640874e5e1a54e908703e91b5c478ad25
Reto Schneider <github@reto-schneider.ch>
2012-07-09 16:48:03 +0200

    Refactoring Part #1:   - Allow more than one map on a single site (remove global variables)   - Integrate many js files into area.js   - Make more stuff configurable via theme   - Allow the map to be resized by the user   - Use thousands separator for CH1903 coordinates

--- webroot_drupal/sites/all/modules/area/area.tpl.php
+++ webroot_drupal/sites/all/modules/area/area.tpl.php
@@ -3,25 +3,34 @@
  * @author Reto Schneider, 2012
  * @copyright Naturwerk
  * @file area.tpl.php
+ * @note I created may different javascript files for the following reason:
+ *  - better structured, easier to manage
+ * @note I did not use the drupal drupal_add_css/drupal_add_js because it's easier
+ * to use this map in an overlay. Feel free to use the drupal_* functions and it's
+ * goodies if you know how to make it working in an overlay.
  */
+
+$mapid = 'area_' . $mapid; // make sure $mapid has an 'area_' prefix, avoid clashes
+$mapid_canvas = $mapid . '_canvas';
+
 $style = '';
 if ($height || $width) {
 	$style = ' style="';
 	if ($height)
 		$style .= 'height: ' . $height . '; ';
 	if ($width)
-		$style .= 'height: ' . $width . '; ';
+		$style .= 'width: ' . $width . '; ';
 	$style .= '"';
 }
 ?>
-<div class="area-search-map">
-  <div id="map">
-    <div id="map_canvas" <?php echo $style; ?> >
-		<h1> <?php print(t('You have to enable Javascript!')) ?> </h1>
-    </div>
-  </div>
+
+<div id="<?php echo $mapid ?>">
+	<div id="<?php echo $mapid_canvas ?>" <?php echo $style; ?>>
+    	<h1><?php print(t('You have to enable Javascript!')) ?></h1>
+	</div>
 </div>
 <?php
+global $user;
 // include CSS ...
 function area_add_js_url($url) {
 	print "<script type='text/javascript' src='$url'></script>\n";
@@ -36,7 +45,7 @@ $baseModulPath = base_path() . drupal_get_path('module', 'area') . '/';
 $baseModulJsPath = $baseModulPath . 'js/';
 $baseModulCssPath = $baseModulPath . 'css/';
 
-global $user;
+drupal_add_library('system', 'ui.resizable');
 
 /**
  * Options set for the google maps api:
@@ -60,30 +69,9 @@ if ($action == 'create' || $action == 'getcoordinate') {
 $area_protocol = isset($_SERVER['HTTPS']) && !empty($_SERVER['HTTPS'])
 		? 'https://' : 'http://';
 area_add_js_url(
-	$area_protocol . "maps.google.com/maps/api/js?sensor=false&libraries=$libraries&region=CH&language="
+	$area_protocol
+			. "maps.google.com/maps/api/js?sensor=false&libraries=$libraries&region=CH&language="
 			. $user->language . "\n");
-
-/**
- * To edit a geometry, the area_id has to be set in javascript.
- */
-if ($area_id > 0) {
-	// should be included before area.js
-	print "<script>areaid = $area_id;</script>\n";
-}
-/**
- * To edit a geometry, the area_id has to be set in javascript.
- */
-if ($json_url) {
-	// should be included before area.js
-	print "<script>json_url = '$json_url';</script>\n";
-}
-
-/**
- * If requested, include an scale on the map.
- */
-print 
-	"<script>scalecontrol = " . ($scalecontrol ? "true" : "false")
-			. ";</script>\n";
 
 area_add_js_url($baseModulJsPath . 'contrib/v3_epoly_sphericalArea.js');
 area_add_js_url($baseModulJsPath . 'area-googlemapsapi-extensions.js');
@@ -92,18 +80,10 @@ area_add_js_url($baseModulJsPath . 'area.js');
 area_add_css_url($baseModulCssPath . 'area-theme.css');
 
 /**
- * Include a searchbar if requested.
- */
-if ($search) {
-	area_add_js_url($baseModulJsPath . 'area-search.js');
-}
-
-/**
- * Include a searchbar/display for 1903 coordinates if requested.
+ * Include contributed functions needed by the CH1903 box.
  */
 if ($ch1903) {
 	area_add_js_url($baseModulJsPath . 'contrib/wgs84_ch1903.js');
-	area_add_js_url($baseModulJsPath . 'area-ch1903.js');
 }
 
 /**
@@ -111,58 +91,25 @@ if ($ch1903) {
  */
 if ($reticle) {
 	$reticle_image_url = base_path() . drupal_get_path('module', 'commonstuff')
-	. '/images/reticle.png';
-	drupal_add_js(
-			array('area' => array('reticleimageurl' => $reticle_image_url)),
-			'setting');
-	area_add_js_url($baseModulJsPath . 'area-reticle.js');
+			. '/images/reticle.png';
 }
 
-/**
- * If existing area should be shown...
- */
-switch ($show) {
-case 'allareas': /**
-				  * Display all of them
-				  */
-	area_add_js_url($baseModulJsPath . 'area-show-allareas.js');
-	break;
-case 'myareas': /**
-				 * Display just the ones the user owns.
-				 */
-	area_add_js_url($baseModulJsPath . 'area-show-myareas.js');
-	break;
-case 'custom-show': /**
-					 * Display just the ones the user owns.
-					 */
-	area_add_js_url($baseModulJsPath . 'area-show-geometry.js');
-	break;
-}
-
-/**
- * Allow the user of this theme to set a hidden filed to store the coordinates
- * (encoded as JSON string)
- */
-if ($coordinate_storage_id != false) {
-	print 
-		"<script>coordinate_storage_id = '$coordinate_storage_id';</script>\n";
-}
+// /**
+//  * If existing area(s) should be shown...
+//  */
+// switch ($show) {
+// /**
+//  * Display just the ones the user owns.
+//  */
+// case 'custom-show':
+// 	area_add_js_url($baseModulJsPath . 'area-show-geometry.js');
+// 	break;
+// }
 
 /**
  * Decide which actions should be exectured...
  */
 switch ($action) {
-case 'create': /**
-			    * Create a new area
-			    */
-	area_add_js_url($baseModulJsPath . 'area-create.js');
-	break;
-/**
- * Create an existing area geometry
- */
-case 'edit':
-	area_add_js_url($baseModulJsPath . 'area-edit.js');
-	break;
 /**
  * Create an existing area geometry
  */
@@ -177,3 +124,77 @@ case 'getcoordinate':
 	break;
 }
 ?>
+<!-- ----------------------------------------------- -->
+<?php
+
+/**
+ * Allow the user of this theme to set a hidden field to store the coordinates
+ * (encoded as JSON string)
+ */
+if ($coordinate_storage_id != false) {
+	print 
+		"<script>coordinate_storage_id = '$coordinate_storage_id';</script>\n";
+}
+
+/**
+ * To edit a geometry, the area_id has to be set in javascript.
+ */
+if ($json_url) {
+	// should be included before area.js
+	print "<script>json_url = '$json_url';</script>\n";
+}
+?>
+<script>
+	jQuery(document).ready(function() {
+		var options = {
+				canvasid: '<?php echo $mapid_canvas ?>',
+				search: <?php echo $search ? 1 : 0 ?>,
+				ch1903: <?php echo $ch1903 ? 1 : 0 ?>,
+				mapswitch: <?php echo $mapswitch ? 1 : 0 ?>,
+				mapswitchlevel: <?php echo $mapswitchlevel ? 1 : 0 ?>,
+				loadlastlocation: <?php echo $loadlastlocation ? 1 : 0 ?>,
+				savelastlocation: <?php echo $savelastlocation ? 1 : 0 ?>,
+				width: '<?php echo $width ?>',
+				height: '<?php echo $height ?>',
+				drawingmanager: <?php echo $action == 'create' ? 1 : 0 ?>,
+				geometryedit: <?php echo $action == 'edit' ? 1 : 0 ?>,
+				geometryeditid: <?php echo $geometry_edit_id ? $geometry_edit_id
+		: 0
+								?>,
+				reticle: <?php echo $reticle ? 1 : 0 ?>,
+				reticleimageurl: '<?php echo $reticle_image_url ?>',
+				coordinatestorageid: <?php echo $coordinate_storage_id
+		? $coordinate_storage_id : 0
+									 ?>,
+				googlemapsoptions: {
+						zoom: <?php echo $defaultzoom ?>,
+						streetViewControl: <?php echo $streetview ? 1 : 0 ?>,
+						overviewMapControl: <?php echo $mapcontrol ? 1 : 0 ?>,
+						scaleControl: <?php echo $scalecontrol ? 1 : 0 ?>,
+						scrollwheel: <?php echo $scrollwheel ? 1 : 0 ?>,
+						center: new google.maps.LatLng(46.77373, 8.25073),
+						mapTypeId : google.maps.MapTypeId.ROADMAP,
+				}
+			};
+		if (jQuery('#' + options.canvasid).length) {
+			var <?php echo $mapid ?> = new Area(options);
+
+			jQuery( "#<?php echo $mapid_canvas ?>" ).resizable({
+				stop: function(event, ui) {
+					// Update the size of the google map
+					google.maps.event.trigger(<?php echo $mapid ?>.googlemap, "resize");
+					console.log(<?php echo $mapid ?>.googlemap);
+				},
+				maxWidth: jQuery( "#<?php echo $mapid_canvas ?>" ).width(),
+				minHeight: 300,
+				minWidth: 600,
+				// Better alternative, but does not show the cordner:
+				// handles: 'n, s',
+			});
+		} else {
+			// display errormessage to console log
+			errormsg = "HTML element with id '" + options.canvasid + "' not found. No google maps will be displayed.";
+			alert(errormsg);
+		}
+	});
+</script>
