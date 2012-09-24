86a9e62f346e4f4f6d15c38a23704b9179ccbb89
Chakrit Bhamornsiri <mail@chakrit.ch>
2012-09-24 13:31:37 +0200

    bugfix organism gallery preProcess

--- webroot_drupal/sites/all/modules/organism/js/organism.js
+++ webroot_drupal/sites/all/modules/organism/js/organism.js
@@ -2,6 +2,18 @@ jQuery(document).ready(function() {
 	$ = jQuery;
 	organism = {};
 	
+	/**
+	 * Executed before table is populated
+	 * ! must return the data !
+	 */
+	organism.tablePreProcess = function(data) {
+		data = gallery_addon.preProcess('organisms', 'gallery_image', data);
+		return data;
+	};
+	
+	/**
+	 * Go to /organism/classifiction/id if not already there
+	 */
 	organism.loadClassification = function(id) {
 		var path = window.location.pathname.split('/');
 		var currentId = path[path.length-1];
