ec09374e06513b94c6768eb821cd4e744ace14d8
zivi <xy>
2013-03-21 15:05:17 +0100

    Update to remove_user custom module
    Display entries of user in stopforumspam.com database. Code cleanup,
    added comments.

--- webroot_drupal/sites/all/modules/remove_user/admin-foruminfo.tpl.php
+++ webroot_drupal/sites/all/modules/remove_user/admin-foruminfo.tpl.php
@@ -1,4 +1,24 @@
 <?php 
+/**
+* @file
+* Template for Admin User Details
+* in Topic View of Forum section.
+* 
+* Available variables:
+* -$node: Node that represents this topic.
+* -$account: Account that created the node.
+* -$nrOfForumPosts: Number of Forum posts that the user account made.
+* -$nrOfComments: Number of Comments that the user account made.
+* -$dateOfLastForumPost: Date of last forum post that the user acount made.
+* -$dateOfLastComment: Date of last comment that the user acount made.
+* -$usernameappears: Number of times the username appears in the StopForumSpam DB.
+* -$emailappears: Number of times the email address that was used by user account to sign up
+*  appears in the StopForumSpam DB.
+* @see remove_user_theme(), remove_user_node_view()
+*/
+?>
+
+<?php 
 global $user;
 $node = $variables['node'];
 $account = $variables['account'];
@@ -7,27 +27,29 @@ $nrOfForumPosts = $variables['nrOfForumPosts'];
 $nrOfComments = $variables['nrOfComments'];
 $dateOfLastForumPost = $variables['dateOfLastForumPost'];
 $dateOfLastComment = $variables['dateOfLastComment'];
+$roles = $account->roles;
+$usernameappears = $variables['usernameappears'];
+$emailappears = $variables['emailappears'];
+$spam_username_class = $usernameappears > 0 ? 'spam_appears_1': 'spam_appears_0';
+$spam_mail_class = $emailappears > 0 ? 'spam_appears_1': 'spam_appears_0';
 ?>
 
-<!--<?php echo "<pre>";
-print_r($variables);
-echo "</pre>";
-?>-->
-
 <?php if (is_array($user->roles) && in_array('administrator', $user->roles)) { ?>
-<?php /*Code parts taken from user_stats module */ ?>
 	<div class="messages error">
-		<h3>Admin Info</h3>
-		<span class="tabulator">Userid:</span> <?php print($uid); ?><br/>
-		<span class="tabulator">User:</span> <?php echo l($account->name,'user/'.$node->uid);?><br />
-		<span class="tabulator">Account created on:</span> <?php print(format_date($account->created)); ?><br />
-		<span class="tabulator">Days ago:</span> <?php print floor((time() - $account->created) / 86400); ?><br />
-		<span class="tabulator">Last login:</span> <?php print format_date($account->login); ?><br />
-		<span class="tabulator">Nr of comments:</span> <?php print $nrOfComments; ?><br />
-		<span class="tabulator">Forum posts:</span> <?php print $nrOfForumPosts; ?><br/>
-		<span class="tabulator">Last forum post:</span> <?php print format_date($dateOfLastForumPost); ?><br />
-		<span class="tabulator">Last comment:</span> <?php print format_date($dateOfLastComment); ?><br />
-		<!-- <span class="tabulator">TEST</span><?php print l('REMOVE', 'remove/user/uid/'. $account->uid); ?><br /> -->
-		<span class="tabulator">Delete:</span><?php print l('Delete user and content', 'remove/user/uid/confirm/'. $account->uid); ?><br />
+		<h3><?php echo t('Admin Info'); ?></h3>
+		<span class="tabulator"><?php echo t('Userid')?>:</span> <?php print($uid); ?><br/>
+		<span class="tabulator"><?php echo t('User')?>:</span> <?php echo l($account->name,'user/'.$node->uid);?><br />
+		<span class="tabulator"><?php echo t('Roles')?>:</span><?php $i = 0; $arr_count = count($roles); foreach($roles as $role){ if($i > 0){ echo "<br />"; } echo $role; if($i > 0){ echo "<span class=\"tabulator\">&nbsp;</span>"; } $i++; } ?> <br/>
+		<span class="tabulator"><?php echo t('Account created on')?>:</span> <?php print(format_date($account->created)); ?><br />
+		<span class="tabulator"><?php echo t('Days ago')?>:</span> <?php print floor((time() - $account->created) / 86400); ?><br />
+		<span class="tabulator"><?php echo t('Last login')?>:</span> <?php print format_date($account->login); ?><br />
+		<span class="tabulator"><?php echo t('Nr of comments')?>:</span> <?php print $nrOfComments; ?><br />
+		<span class="tabulator"><?php echo t('Forum posts')?>:</span> <?php print $nrOfForumPosts; ?><br/>
+		<span class="tabulator"><?php echo t('Last forum post')?>:</span> <?php print format_date($dateOfLastForumPost); ?><br />
+		<span class="tabulator"><?php echo t('Last comment')?>:</span> <?php print format_date($dateOfLastComment); ?><br />
+		<hr class="hr_style"/>
+		<span class="tabulator"><?php echo t('Username appears')?>:</span> <div class="appearsWrapper"><div class="<?php print $spam_mail_class; ?>">&nbsp;</div><?php print check_plain($usernameappears)?> (<?php print l('StopForumSpam','http://www.stopforumspam.com/search/'. check_plain($account->name)); ?>)</div>
+		<span class="tabulator"><?php echo t('Email appears')?>:</span> <div class="appearsWrapper"><div class="<?php print $spam_username_class; ?>">&nbsp;</div><?php print check_plain($emailappears)?> (<?php print l('StopForumSpam','http://www.stopforumspam.com/search/'. check_plain($account->init)); ?>)</div>
+		<span class="tabulator"><?php echo t('Purge')?>:</span><?php print l('Delete user and content', 'remove/user/uid/confirm/'. $account->uid); ?><br />
 	</div>
 <?php } ?>
