2d2475e0fec0ee34591d5306f51712b515219019
Matthias <matthias.suter@gmail.com>
2013-07-22 09:50:50 +0200

    gallery lightbox adjusted: second gallery for a single observation/organism entry

--- webroot_drupal/sites/all/modules/gallery/js/gallery.lightbox.js
+++ webroot_drupal/sites/all/modules/gallery/js/gallery.lightbox.js
@@ -5,6 +5,11 @@ var galleryLightboxSettings = {
 	imageSelector : 'a:first'
 }; 
 
+var galleryOpenLightboxSettings = {
+		captionSelector : '.caption',
+		captionAttr : false,
+		captionHTML : true,
+}; 
 
 (function($) {
 	
@@ -14,6 +19,7 @@ var galleryLightboxSettings = {
 		gallery_lightbox.registerLightBox();
 	});
 	
+	//lightbox for iteration over multiple entries
 	gallery_lightbox.registerLightBox = function() {
 		$('a[rel^="lightbox"]').lightBox();
 		$('ul.gallery:not(.presentation) li').lightBox(galleryLightboxSettings).find('.caption').hide();
@@ -21,6 +27,12 @@ var galleryLightboxSettings = {
 		$('ul.presentation li .caption a').click(function(e) {
 			e.stopPropagation();
 		});
+	};
+	
+	//lightbox for iteration over images of one entry
+	gallery_lightbox.openLightBoxEntry = function(entryId) {
+		$('div[name=lightbox_entry_' + entryId + '] a[class=]"').lightBox(galleryOpenLightboxSettings);
+		$('div[name=lightbox_entry_' + entryId + '] a[class=]"').first().trigger('click');
 	};
 	
 })(jQuery);
