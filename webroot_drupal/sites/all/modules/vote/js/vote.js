67296d5ea00155bb48b1952ed7d16a5033d1d53b
Emin Khateeb <eminkhateeb@gmail.com>
2013-09-23 08:12:24 +0200

    implemented comment display, fixed bar charts

--- webroot_drupal/sites/all/modules/vote/js/vote.js
+++ webroot_drupal/sites/all/modules/vote/js/vote.js
@@ -15,37 +15,20 @@ $(document).ready(function() {
 function cacheAllData(result) {
 	
 	// cache general information
-	generalInformation = {
-		current_user_id: result.general.current_user_id,
-		translated_labels: result.general.translated_labels
-	};
+	generalInformation = result.general;
 	
 	// cache observation information
-	for (var i = 0; i < result.observations.length; i++) {
-		observations[i] = {
-			observation_id: result.observations[i].observation_id,
-			thumbnail_image_path: result.observations[i].thumbnail_image_path,
-			fullsize_image_path: result.observations[i].fullsize_image_path,
-			observation_images: result.observations[i].observation_images,
-			detail_information: result.observations[i].detail_information,
-			google_map_marker_lat: result.observations[i].google_map_marker_lat,
-			google_map_marker_lng: result.observations[i].google_map_marker_lng,
-		};
+	if(result.observations != null) {
+		for (var i = 0; i < result.observations.length; i++) {
+			observations[i] = result.observations[i];
+		}
 	}
 	
 	// cache verification information
-	for (var i = 0; i < result.verifications.length; i ++) {
-		verifications[i] = {
-			vote_id: result.verifications[i].vote_id,
-			observation_id: result.verifications[i].observation_id,
-			organism_id: result.verifications[i].organism_id,
-			scientific_name: result.verifications[i].scientific_name,
-			translated_name: result.verifications[i].translated_name,
-			vote_timestamp: result.verifications[i].vote_timestamp,
-			comment: result.verifications[i].comment,
-			user_id: result.verifications[i].user_id,
-			user_name: result.verifications[i].user_name
-		};
+	if(result.verifications != null) {
+		for (var i = 0; i < result.verifications.length; i ++) {
+			verifications[i] = result.verifications[i];
+		}
 	}
 }
 
@@ -58,56 +41,34 @@ function initializeSubmitVerificationButton() {
 			type: "POST",
 			url: "vote/save",
 			success: function(result){
-				
 				// scroll to the top of the page to display the next observation
 				$('html, body').animate({ scrollTop: 0 });
 				
-				// TODO: scientific name hidden field
+				var currentVerifications = getVerificationsForObservation(observations[currentMainImageIndex].observation_id);
+				// TODO: make a hidden field for the organism id and the scientific name
 				var myVote = {
-						translated_name: $('#organism_name').val(),
-						scientific_name: $('#organism_name').val(),
-						votes: 1,
-						user_verifications: [{ user_id: generalInformation.current_user_id, vote_comment: $('#comment').val() }]
+					comment: $('#comment').val(),
+					observation_id: observations[currentMainImageIndex].observation_id.toString(),
+					organism_id: "0",
+					scientific_name: $('#organism_name').val(),
+					translated_name: $('#organism_name').val(),
+					user_id: generalInformation.current_user_id.toString(),
+					user_name: generalInformation.current_user_name,
+					vote_timestamp: Math.round((new Date()).getTime() / 1000).toString() 
 				};
 				
-				// don't load observations[currentMainImageIndex].current_verifications into a variable because it will create an instance of the variable and it would not work with the real array
-				if (observations[currentMainImageIndex].current_verifications == null) {
-					observations[currentMainImageIndex].current_verifications = new Array();
+				if(currentVerifications == null) {
+					currentVerifications = [myVote];
 				} else {
-					for (var i = 0; i < observations[currentMainImageIndex].current_verifications.length; i++) {
-						for(var a = 0; a < observations[currentMainImageIndex].current_verifications[i].user_verifications.length; a++) {
-							
-							// check if the current user voted for this suggestion
-							if (generalInformation.current_user_id == observations[currentMainImageIndex].current_verifications[i].user_verifications[a].user_id) {
-								
-								// check if the suggestion, which the user voted for has votes from other users. if yes, then leave the verifications there and just remove 1 verification
-								if (observations[currentMainImageIndex].current_verifications[i].votes > 1) {
-									observations[currentMainImageIndex].current_verifications[i].votes--;
-									observations[currentMainImageIndex].current_verifications[i].user_verifications[a].user_id = null;
-									observations[currentMainImageIndex].current_verifications[i].user_verifications[a].vote_comment = null;
-								} else {
-									observations[currentMainImageIndex].current_verifications.splice(i, 1);
-								}
-								break;
-							}
+					for(var i = 0; i < currentVerifications.length; i++) {
+						if(currentVerifications[i].user_id == generalInformation.current_user_id) {
+							currentVerifications.splice(i, 1);
 						}
 					}
+					currentVerifications.push(myVote);
 				}
 				
-				// check if the verification, which the user selected already exists. if yes, then just count 1 vote up, else push the vote to the verification array
-				var verificationExists = false;
-				for (var i = 0; i < observations[currentMainImageIndex].current_verifications.length; i++) {
-					if (observations[currentMainImageIndex].current_verifications[i].translated_name == myVote.translated_name) {
-						observations[currentMainImageIndex].current_verifications[i].votes++;
-						observations[currentMainImageIndex].current_verifications[i].user_verifications = [{ user_id: generalInformation.current_user_id, vote_comment: $('#comment').val() }];
-						verificationExists = true;
-						break;
-					}
-				}
-			
-				if (!verificationExists) {
-					observations[currentMainImageIndex].current_verifications.push(myVote);
-				}
+				setVerificationsForObservation(observations[currentMainImageIndex].observation_id, currentVerifications);
 				
 				nextImage();
 			},
@@ -135,26 +96,37 @@ function getVerificationsForObservation(observation_id) {
 	return resultArray;
 }
 
+function setVerificationsForObservation(observation_id, newVerificationsArray) {
+	for (var i = 0; i < verifications.length; i ++) {
+		if (verifications[i].observation_id == observation_id) {
+			verifications.splice(i, 1);
+		}
+	}
+	
+	for (var i = 0; i < newVerificationsArray.length; i ++) {
+		verifications.push(newVerificationsArray[i]);
+	}
+}
+
 function getVerificationsPerOrganism(filteredVerificationsArray) {
+	var alreadyPushedOrganismIds = [];
 	var resultArray = [];
 	
-	for(var i = 0; i < filteredVerificationsArray.length; i++) {
-		if(resultArray.length > 0) {
-			for(var a = 0; a < resultArray.length; a++) {
-				if(resultArray[a].organism_id == filteredVerificationsArray[i].organism_id) {
-					resultArray[a].verifications.push(filteredVerificationsArray[i]);
-				} else {
-					resultArray.push({
-						organism_id: filteredVerificationsArray[i].organism_id,
-						verifications: [filteredVerificationsArray[i]]
-					});
-				}
-			}
-		} else {
+	for (var i = 0; i < filteredVerificationsArray.length; i++) {
+		var currentOrganismId = filteredVerificationsArray[i].organism_id;
+		if (alreadyPushedOrganismIds.indexOf(currentOrganismId) == -1) {
 			resultArray.push({
-				organism_id: filteredVerificationsArray[i].organism_id,
+				organism_id: currentOrganismId,
 				verifications: [filteredVerificationsArray[i]]
 			});
+			alreadyPushedOrganismIds.push(currentOrganismId);
+		} else {
+			for (var b = 0; b < resultArray.length; b++) {
+				if (currentOrganismId == resultArray[b].organism_id) {
+					resultArray[b].verifications.push(filteredVerificationsArray[i]);
+					break;
+				}
+			}
 		}
 	}
 	
@@ -182,12 +154,12 @@ function initializeVotesFromOtherUsers() {
 		var totalVotes = currentVerifications.length;
 		
 		var verificationsPerOrganism = getVerificationsPerOrganism(currentVerifications);
+		var htmlString = "";
 		
 		for (var i = 0; i < verificationsPerOrganism.length; i++) {
 			var votesForCurrentOrganism = verificationsPerOrganism[i].verifications.length;
 			var votesPercent = Math.round(votesForCurrentOrganism / totalVotes * 100, 0);
 			
-			var htmlString = "";
 			htmlString +=		'<div class="entry">'
 							  + '<div class="progressBar" style="width: ' + votesPercent + '%;">'
 							  + '<span class="translatedDescription">' + verificationsPerOrganism[i].verifications[0].translated_name + '</span>'
@@ -198,16 +170,16 @@ function initializeVotesFromOtherUsers() {
 							  + '<div class="entryCommentsContainer">';
 			
 			for (var a = 0; a < verificationsPerOrganism[i].verifications.length; a++) {
-			htmlString +=		'<div class="entryComments">'
-							  + '<p class="header"><b>' + verificationsPerOrganism[i].verifications[a].user_name + '</b> am ' + verificationsPerOrganism[i].verifications[a].vote_timestamp + ':</p>'
-							  + '<p>' + verificationsPerOrganism[i].verifications[a].comment + '</p>'
-							  + '</div>';
+				htmlString +=		'<div class="entryComments">'
+								  + '<p class="header"><b>' + verificationsPerOrganism[i].verifications[a].user_name + '</b> am ' + new Date(verificationsPerOrganism[i].verifications[a].vote_timestamp * 1000).format("d.m.Y H:i") + ':</p>'
+								  + '<p>' + verificationsPerOrganism[i].verifications[a].comment + '</p>'
+								  + '</div>';
 			}
+			
 			htmlString +=		'</div>'
 							  + '</div>';
-			
-			container.html(htmlString);
 		}
+		container.html(htmlString);
 	} else {
 		
 		// hide container and display information message
@@ -218,3 +190,6 @@ function initializeVotesFromOtherUsers() {
 	
 	initializeSelectBox();
 }
+
+//Simulates PHP's date function
+Date.prototype.format=function(e){var t="";var n=Date.replaceChars;for(var r=0;r<e.length;r++){var i=e.charAt(r);if(r-1>=0&&e.charAt(r-1)=="\\"){t+=i}else if(n[i]){t+=n[i].call(this)}else if(i!="\\"){t+=i}}return t};Date.replaceChars={shortMonths:["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"],longMonths:["January","February","March","April","May","June","July","August","September","October","November","December"],shortDays:["Sun","Mon","Tue","Wed","Thu","Fri","Sat"],longDays:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],d:function(){return(this.getDate()<10?"0":"")+this.getDate()},D:function(){return Date.replaceChars.shortDays[this.getDay()]},j:function(){return this.getDate()},l:function(){return Date.replaceChars.longDays[this.getDay()]},N:function(){return this.getDay()+1},S:function(){return this.getDate()%10==1&&this.getDate()!=11?"st":this.getDate()%10==2&&this.getDate()!=12?"nd":this.getDate()%10==3&&this.getDate()!=13?"rd":"th"},w:function(){return this.getDay()},z:function(){var e=new Date(this.getFullYear(),0,1);return Math.ceil((this-e)/864e5)},W:function(){var e=new Date(this.getFullYear(),0,1);return Math.ceil(((this-e)/864e5+e.getDay()+1)/7)},F:function(){return Date.replaceChars.longMonths[this.getMonth()]},m:function(){return(this.getMonth()<9?"0":"")+(this.getMonth()+1)},M:function(){return Date.replaceChars.shortMonths[this.getMonth()]},n:function(){return this.getMonth()+1},t:function(){var e=new Date;return(new Date(e.getFullYear(),e.getMonth(),0)).getDate()},L:function(){var e=this.getFullYear();return e%400==0||e%100!=0&&e%4==0},o:function(){var e=new Date(this.valueOf());e.setDate(e.getDate()-(this.getDay()+6)%7+3);return e.getFullYear()},Y:function(){return this.getFullYear()},y:function(){return(""+this.getFullYear()).substr(2)},a:function(){return this.getHours()<12?"am":"pm"},A:function(){return this.getHours()<12?"AM":"PM"},B:function(){return Math.floor(((this.getUTCHours()+1)%24+this.getUTCMinutes()/60+this.getUTCSeconds()/3600)*1e3/24)},g:function(){return this.getHours()%12||12},G:function(){return this.getHours()},h:function(){return((this.getHours()%12||12)<10?"0":"")+(this.getHours()%12||12)},H:function(){return(this.getHours()<10?"0":"")+this.getHours()},i:function(){return(this.getMinutes()<10?"0":"")+this.getMinutes()},s:function(){return(this.getSeconds()<10?"0":"")+this.getSeconds()},u:function(){var e=this.getMilliseconds();return(e<10?"00":e<100?"0":"")+e},e:function(){return"Not Yet Supported"},I:function(){var e=null;for(var t=0;t<12;++t){var n=new Date(this.getFullYear(),t,1);var r=n.getTimezoneOffset();if(e===null)e=r;else if(r<e){e=r;break}else if(r>e)break}return this.getTimezoneOffset()==e|0},O:function(){return(-this.getTimezoneOffset()<0?"-":"+")+(Math.abs(this.getTimezoneOffset()/60)<10?"0":"")+Math.abs(this.getTimezoneOffset()/60)+"00"},P:function(){return(-this.getTimezoneOffset()<0?"-":"+")+(Math.abs(this.getTimezoneOffset()/60)<10?"0":"")+Math.abs(this.getTimezoneOffset()/60)+":00"},T:function(){var e=this.getMonth();this.setMonth(0);var t=this.toTimeString().replace(/^.+ \(?([^\)]+)\)?$/,"$1");this.setMonth(e);return t},Z:function(){return-this.getTimezoneOffset()*60},c:function(){return this.format("Y-m-d\\TH:i:sP")},r:function(){return this.toString()},U:function(){return this.getTime()/1e3}}
