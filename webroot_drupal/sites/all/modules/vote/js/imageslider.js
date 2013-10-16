2a7a56399f3f3c733eb03afadd0c997d251c9ee6
André Kälin <kaelin.andre@gmail.com>
2013-10-16 08:11:52 +0200

    after reaching last image, but there are still more available, we do not begin again with the first picture. (only if no more images can be fetched)

--- webroot_drupal/sites/all/modules/vote/js/imageslider.js
+++ webroot_drupal/sites/all/modules/vote/js/imageslider.js
@@ -84,7 +84,7 @@ function initializeCache() {
 	});
 }
 
-function fetchNextPage(steps, replaceMainImage) {
+function fetchNextImages(steps, replaceMainImage) {
 	page++;
 	$.ajax({
 		url: "/vote/getdata/json/" + page,
@@ -135,8 +135,12 @@ function loadImagesFromCache() {
 
 		// load next thumbnail and fullsize images into cache
 		cleanIndex = checkIndex(imageIndex + i);
-		currentImageHolders[i].attr('src', observations[cleanIndex].thumbnail_image_path);
-		$('#mainImageContainer').append("<img src=\"" + observations[cleanIndex].fullsize_image_path + "\" style=\"display:none;\" alt=\"Image\" />");
+		if(imageIndex + i > observations.length && imageIndex + i < generalInformation['total']) {
+			
+		} else { //display image (restarting at the beginning, if no more images can be fetched from the server
+			currentImageHolders[i].attr('src', observations[cleanIndex].thumbnail_image_path);
+			$('#mainImageContainer').append("<img src=\"" + observations[cleanIndex].fullsize_image_path + "\" style=\"display:none;\" alt=\"Image\" />");
+		}
 
 		// load future thumbnail and fullsize images into cache
 		cleanIndex = checkIndex(imageIndex + numberToCount + i);
@@ -259,7 +263,7 @@ function moveImages(steps, replaceMainImage) {
 	//if imageIndex is greater then pagesize, and more images are available on server, fetch next page
 	if(imageIndex + steps >= page*pageSize && page*pageSize < parseInt(generalInformation['total'])) {
 		navigationDisabled = true;
-		fetchNextPage(steps, replaceMainImage);
+		fetchNextImages(steps, replaceMainImage);
 		return;
 	}
 	stepsToMove = steps;
